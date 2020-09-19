848582950171995b1d92fa4eaf4d82877ba96834
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-09-19 16:52:27 +0100

    Update em_bet_fast.sh
    Added WL site specific BET settings

--- em_bet_fast.sh
+++ em_bet_fast.sh
@@ -7,6 +7,12 @@
 # Objectives
 # Enable simple statistics of volumes in native T1
 # Enable calculation of mean DWI / myelin metrics 
+# bash em_bet_fast.sh ${subject} "site" (i.e. WL)
+
+# Updates
+# 17.9.2029. BET was failing on WL subjects (i.e. excessive neck). 
+#            Added "-B -S -R" to BET command for WL subjects.
+
 
 # Module
 module unload fsl/5.0.10
@@ -15,12 +21,20 @@ module load fsl/6.0.1
 # Subject
 i=${1}
 cd ${i}
+site=${2}
 
 # Parameters
 t1_bfc="T1w_${i}_BFC.nii"
 
+if [ "${site}" = "WL" ]; then
+# Run BET with WL settings
+echo "${t1_bfc} ${t1_bfc}_brain -B -S -R"
+bet ${t1_bfc} ${t1_bfc}_brain -B -S -R
+else
 # Run BET with default settings
+echo "${t1_bfc} ${t1_bfc}_brain"
 bet ${t1_bfc} ${t1_bfc}_brain
+fi
 
 # Run FAST
 fast ${t1_bfc}_brain
