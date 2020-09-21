7a67a3796230273c3c7af75388328f9738e0b52c
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-09-21 15:16:49 +0100

    Create em_hpc_diffusion_prevent_batch.sh

---
+++ em_hpc_diffusion_prevent_batch.sh
@@ -0,0 +1,37 @@
+# Job submission for diffusion preprocessing on HPC
+#!/bin/bash
+# Author: Elijah Mak
+# Date: 16th September 2020
+
+# Estimated time on HPC is 35 minutes
+
+# Time
+
+time="00:35:00"
+
+# Memory
+
+mem="2500"
+
+# Number of tasks
+
+nTask=1
+
+# List of subjects to process
+
+input=${1}
+BET=${2}
+
+# Execute job submission
+
+for subject in `cat $input`;
+
+do
+
+sbatch --exclusive --job-name=edc --account MAK-SL3-GPU --partition pascal --error=${subject%.*}_%j.err \
+--output=${subject%.*}_%j.out --time=${time} --nodes=1 --ntasks-per-node=$nTask --mem=$mem \
+--wrap="bash /home/fkm24/rds/hpc-work/prevent_700/em_hpc_diffusion_prevent.sh $subject $BET" --mail-type=ALL
+
+sleep 1
+
+done
