5893da18e556b4ec92c99a37cd08907b6f2d3d8f
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-09-21 13:33:33 +0100

    Update em_bet_fast.sh

--- em_bet_fast.sh
+++ em_bet_fast.sh
@@ -12,6 +12,7 @@
 # Updates
 # 17.9.2029. BET was failing on WL subjects (i.e. excessive neck). 
 #            Added "-B -S -R" to BET command for WL subjects.
+# 21.9.2029. Added FOV as a setting to deal with large FOV.
 
 
 # Module
@@ -21,15 +22,21 @@ module load fsl/6.0.1
 # Subject
 i=${1}
 cd ${i}
-site=${2}
+setting=${2}
 
 # Parameters
 t1_bfc="T1w_${i}_BFC.nii"
 
-if [ "${site}" = "WL" ]; then
+if [ "${setting}" = "WL" ]; then
 # Run BET with WL settings
 echo "${t1_bfc} ${t1_bfc}_brain -B -S -R"
 bet ${t1_bfc} ${t1_bfc}_brain -B -S -R
+else if [ "${setting}" = "FOV" ]
+# Run BET with large FOV settings
+echo "robustfov -i ${t1_bfc} -r T1w_WL001_BFC_FOV_crop"
+robustfov -i ${t1_bfc} -r T1w_${i}_BFC_FOV_crop
+echo "bet T1w_${i}_BFC_FOV_crop ${t1_bfc}_brain -R"
+bet T1w_${i}_BFC_FOV_crop ${t1_bfc}_brain -R
 else
 # Run BET with default settings
 echo "${t1_bfc} ${t1_bfc}_brain"
