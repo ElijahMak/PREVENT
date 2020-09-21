3591870a1d3b7f08e6ad09db84116f729b351628
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-09-21 15:22:07 +0100

    em_hpc_mdt_prevent_batch.sh

---
+++ em_hpc_mdt_prevent_batch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This is the job submission script to perform NODDI fitting using the MDT toolbox for all PREVENT subjects.
+
+# Number of tasks
+nTask=1
+
+# Time
+time="02:59:0"
+input=${1}
+
+for subject in `cat $input`;
+
+do
+
+sbatch --exclusive --job-name=noddi --account MAK-SL3-GPU --partition pascal --error=${subject%.*}_%j.err \
+--output=${subject%.*}_%j.out --time=${time} --nodes=1 --ntasks-per-node=$nTask --mem=2500 \
+--wrap="bash /home/fkm24/rds/hpc-work/prevent_700/em_hpc_mdt_prevent.sh $subject 1" --mail-type=ALL
+
+sleep 1
+
+done
