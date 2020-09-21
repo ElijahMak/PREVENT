31af32e54fc1a5e41e4cdfff73aa316414deb9ae
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-09-21 15:29:36 +0100

    Create em_hpc_mdt_prevent.sh

---
+++ em_hpc_mdt_prevent.sh
@@ -0,0 +1,27 @@
+subject=${1}
+cd ${subject}
+
+# Running NODDI
+module load mdt/0.21.0
+module load miniconda3/4.5.1
+
+# Parameters
+mask="b0_brain_mask.nii.gz"
+input="edc.repol.s2v.nii.gz"
+rotated_bvecs="edc.repol.s2v.eddy_rotated_bvecs"
+bval="bval"
+
+# Create protocol
+python $(which mdt-create-protocol) $rotated_bvecs $bval -o mdt.prtcl
+prtcl="mdt.prtcl"
+rm -rf /home/fkm24/.cache/pyopencl/pyopencl-compiler-cache-v2-py3.6.5.final.0/lock
+
+echo "Fitting NODDI WM"
+mkdir mdt_WM
+echo "python $(which mdt-model-fit) NODDI $input $prtcl ${mask} -o mdt_WM"
+python $(which mdt-model-fit) NODDI $input $prtcl ${mask} -o mdt_WM
+
+echo "Fitting NODDI GM"
+mdt_GM
+echo "python $(which mdt-model-fit) NODDI_GM $input $prtcl ${mask} -o mdt_GM"
+python $(which mdt-model-fit) NODDI_GM $input $prtcl ${mask} -o mdt_GM
