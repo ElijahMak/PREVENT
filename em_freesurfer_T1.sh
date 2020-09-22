417734fb4cb4f0d55016a3d9d2d19d34d8dd411d
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-09-22 16:56:16 +0100

    Create em_freesurfer_T1.sh

---
+++ em_freesurfer_T1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Load modules
+. /etc/profile.d/modules.sh
+module purge
+module load default-wbic
+module load openmpi/3.0.0
+module load freesurfer/7.1.0
+
+# Directory
+export SUBJECTS_DIR="/archive/p00423/PREVENT_Elijah/Freesurfer7_T1"
+subject=${1}
+
+# Recon-all
+recon-all -s ${subject} \
+-i ${SUBJECTS_DIR}/T1w_${subject}.nii \
+-all \
+-openmp 4
