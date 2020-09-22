9e1c2d77ac8fb022d401412406d920ab5c6ee283
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-09-22 17:35:32 +0100

    Create em_freesurfer_T1FLAIR_batch.sh

---
+++ em_freesurfer_T1FLAIR_batch.sh
@@ -0,0 +1,30 @@
+# Job submission for Freesurfer T1 FLAIR on WBIC
+
+#!/bin/bash
+
+# Author: Elijah Mak
+# Date: 22nd September 2020
+
+# Estimated time on HPHI is _ minutes
+
+# Time
+time="10:00:00"
+
+# Memory
+mem="4000"
+
+# Number of tasks
+nTask=1
+
+# List of subjects to process
+input=${1}
+
+# Execute job submission
+for subject in `cat $input`; do 
+
+sbatch --job-name=fs7_t1t2 --account hphi --qos=day.q --partition wbic-cs --error=${subject%.*}_%j.err \
+--output=${subject%.*}_%j.out --time=${time} --nodes=1 --ntasks-per-node=${nTask} --mem=${mem} \
+--wrap="bash ${code}/em_freesurfer_T1FLAIR.sh ${subject}" --mail-type=ALL
+
+sleep 1
+done
