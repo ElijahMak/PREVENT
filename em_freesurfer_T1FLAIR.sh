449dfd1f993396f50a6e881328e7e3f4af638f7b
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-09-22 17:34:52 +0100

    Create em_freesurfer_T1FLAIR.sh

---
+++ em_freesurfer_T1FLAIR.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Load modules
+. /etc/profile.d/modules.sh
+module purge
+module load default-wbic
+module load openmpi/3.0.0
+module load freesurfer/7.1.0
+
+# Directory
+export SUBJECTS_DIR="/lustre/archive/p00423/PREVENT_Elijah/Freesurfer7_T1FLAIR"
+subject=${1}
+
+# Recon-all
+recon-all -s ${subject} \
+-i ${SUBJECTS_DIR}/T1w_${subject}.nii \
+-T2 ${SUBJECTS_DIR}/FLAIR_${subject}.nii \
+-T2pial
+-all \
+-openmp 4
