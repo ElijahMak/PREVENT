6cd105c4f20d34bd95eff834fcefae3254239f39
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-09-25 16:48:29 +0100

    Update em_data_extract_2.sh

--- em_data_extract_2.sh
+++ em_data_extract_2.sh
@@ -5,14 +5,15 @@ cd $subject
 fa="FAtoT1_NMI.nii.gz"
 gm_mask="p1T1w_${subject}_mask_20.nii"
 wm_mask="p2T1w_${subject}_mask_50.nii"
+
 outputfile="/archive/p00423/PREVENT_Elijah/data/metrics.txt"
 
-if [[ `ls * | wc -l` -lt 3  ]]
+if [ `ls * | wc -l` -lt 3  ]
 then
   echo subject >> $outputfile
   echo ${subject} >> $outputfile
   echo fa_gm >> $outputfile
-    echo "files missing" >> $outputfile
+  echo "files missing" >> $outputfile
   echo fa_wm >> $outputfile
   echo "files missing" >> $outputfile
 else 
@@ -20,7 +21,7 @@ else
   echo ${subject} >> $outputfile
   echo fa_gm >> $outputfile
   fslstats -t ${fa} -k ${gm_mask} -M >> $outputfile
-  echo fa_wm >> outputfile.txt
+  echo fa_wm >> $outputfile.txt
   fslstats -t ${fa} -k ${wm_mask} -M >> $outputfile
   fi 
 
