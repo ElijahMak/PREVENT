8b332aa014845cad707b3398616408cf10e196db
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-09-25 15:53:18 +0100

    Update em_data_extract_2.sh

--- em_data_extract_2.sh
+++ em_data_extract_2.sh
@@ -5,6 +5,7 @@ gm_mask="/lustre/archive/p00423/PREVENT_Elijah/CAT12/mri/p1T1w_${subject}_mask_2
 wm_mask="/lustre/archive/p00423/PREVENT_Elijah/CAT12/mri/p2T1w_${subject}_mask_50.nii"
 
 echo subject >> metrics_file.txt
+echo ${subject} >> metrics_file.txt
 echo ${i} >> metrics_file.txt
 echo fa_gm >> metrics_file.txt
 fslstats -t ${fa} -k ${gm_mask} -M >> metrics_file.txt
