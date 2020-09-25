c169c0a69ad1af280e076a319b4863ccdfab8db9
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-09-25 16:36:14 +0100

    Update em_data_extract_2.sh

--- em_data_extract_2.sh
+++ em_data_extract_2.sh
@@ -6,19 +6,23 @@ cd $subject
 fa="FAtoT1_NMI.nii.gz"
 gm_mask="p1T1w_${subject}_mask_20.nii"
 wm_mask="p2T1w_${subject}_mask_50.nii"
+outputfile="/archive/p00423/PREVENT_Elijah/data/metrics.txt"
 
-if [ `ls * | wc -l` -lt 3  ]; then 
-echo subject >> metrics_file.txt
-echo ${subject} >> metrics_file.txt
-echo fa_gm >> metrics_file.txt
-echo "files missing" >> metrics_file.txt
-echo fa_wm >> metrics_file.txt
-echo "files missing" >> metrics_file.txt
+if [ `ls * | wc -l` -lt 3  ]
+then 
+echo subject >> $outputfile
+echo ${subject} >> $outputfile
+echo fa_gm >> $outputfile
+echo "files missing" >>$outputfile
+echo fa_wm >> $outputfile
+echo "files missing" >>$outputfile
 else 
-echo subject >> metrics_file.txt
-echo ${subject} >> metrics_file.txt
-echo $(fslstats -t ${fa} -k ${gm_mask} -M) >> metrics_file.txt
+echo subject >>$outputfile
+echo ${subject} >>$outputfile
+fslstats -t ${fa} -k ${gm_mask -M >>$outputfile
 echo fa_wm >> metrics_file.txt
-echo $(fslstats -t ${fa} -k ${wm_mask} -M) >> metrics_file.txt
+fslstats -t ${fa} -k ${wm_mask} -M >> $outputfile
+
 fi
+
 done
