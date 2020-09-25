687aba31e545bf34390a57804f3d74f003e556e2
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-09-25 16:22:16 +0100

    Update em_data_extract_2.sh

--- em_data_extract_2.sh
+++ em_data_extract_2.sh
@@ -8,6 +8,7 @@ gm_mask="p1T1w_${subject}_mask_20.nii"
 wm_mask="p2T1w_${subject}_mask_50.nii"
 
 if [ ls * | wc -l < 3 ]; then
+
 echo subject >> metrics_file.txt
 echo ${subject} >> metrics_file.txt
 echo fa_gm >> metrics_file.txt
@@ -20,5 +21,5 @@ echo ${subject} >> metrics_file.txt
 echo $(fslstats -t ${fa} -k ${gm_mask} -M) >> metrics_file.txt
 echo fa_wm >> metrics_file.txt
 echo $(fslstats -t ${fa} -k ${wm_mask} -M) >> metrics_file.txt
-
+fi
 done
