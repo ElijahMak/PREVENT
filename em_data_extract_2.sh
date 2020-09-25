0fa384c7d2ce5d7d81126f2d64e3d84f0806d55f
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-09-25 16:55:44 +0100

    Update em_data_extract_2.sh

--- em_data_extract_2.sh
+++ em_data_extract_2.sh
@@ -8,7 +8,7 @@ wm_mask="p2T1w_${subject}_mask_50.nii"
 
 outputfile="/lustre/archive/p00423/PREVENT_Elijah/data/metrics.txt"
 
-if `ls * | wc -l` -lt 3  
+if [ `ls . | wc -l` -lt 3  ]
 then
   echo subject >> $outputfile
   echo ${subject} >> $outputfile
