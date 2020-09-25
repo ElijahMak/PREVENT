d06e74f406de348d1480e721ffad69036b066ba1
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-09-25 16:13:34 +0100

    Update em_data_extract_2.sh

--- em_data_extract_2.sh
+++ em_data_extract_2.sh
@@ -1,12 +1,22 @@
+
 for subject in `cat list`; do
 
-fa="/lustre/archive/p00423/PREVENT_Elijah/FAST/${subject}/FAtoT1_NMI.nii.gz"
-gm_mask="/lustre/archive/p00423/PREVENT_Elijah/CAT12/mri/p1T1w_${subject}_mask_20.nii"
-wm_mask="/lustre/archive/p00423/PREVENT_Elijah/CAT12/mri/p2T1w_${subject}_mask_50.nii"
+cd $subject
 
+fa="FAtoT1_NMI.nii.gz"
+gm_mask="p1T1w_${subject}_mask_20.nii"
+wm_mask="p2T1w_${subject}_mask_50.nii"
+
+if [ ls * | wc -l < 3 ]; then
 echo subject >> metrics_file.txt
 echo ${subject} >> metrics_file.txt
 echo fa_gm >> metrics_file.txt
+echo "files missing" >> metrics_file.txt
+echo fa_wm >> metrics_file.txt
+echo "files missing" >> metrics_file.txt
+else 
+echo subject >> metrics_file.txt
+echo ${subject} >> metrics_file.txt
 echo $(fslstats -t ${fa} -k ${gm_mask} -M) >> metrics_file.txt
 echo fa_wm >> metrics_file.txt
 echo $(fslstats -t ${fa} -k ${wm_mask} -M) >> metrics_file.txt
