5bbe5e69586572bc75ce4b68ad57391f10e67e5c
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-09-25 15:45:27 +0100

    Update em_data_extract_2.sh

--- em_data_extract_2.sh
+++ em_data_extract_2.sh
@@ -1,6 +1,6 @@
 fa="/lustre/archive/p00423/PREVENT/FAST/${subject}/FAtoT1_NMI.nii.gz"
-gm_mask="/lustre/archive/p00423/PREVENT/CAT12/mri/p1T1w_${i}_mask_20.nii.gz"
-wm_mask="/lustre/archive/p00423/PREVENT/CAT12/mri/p2T1w_${i}_mask_50.nii.gz"
+gm_mask="/lustre/archive/p00423/PREVENT/CAT12/mri/p1T1w_${i}_mask_20.nii"
+wm_mask="/lustre/archive/p00423/PREVENT/CAT12/mri/p2T1w_${i}_mask_50.nii"
 
 for subject in `cat list`; do
 echo subject >> metrics_file.txt
