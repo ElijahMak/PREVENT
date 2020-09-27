86188b080aaa86961373a025d02492f5a48e54f2
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-09-27 16:22:41 +0100

    a

--- em_cat12_bet.sh
+++ em_cat12_bet.sh
@@ -1,3 +1,8 @@
-fslmaths p1T1w_${subject} -add p2T1w_${subject} cat12_brain.nii
-mri_binarize --i cat12_brain.nii --min 0.001 --o cat12_brain_mask.nii
-fslmaths T1w_${subject} -mul cat12_brain_mask.nii T1w_${subject}_cat12_brain
+# Generate brain mask from CAT12 segmentations
+module load freesurfer/7.1.0
+
+subject=${1}
+
+fslmaths p1T1w_${subject} -add p2T1w_${subject} ${subject}_cat12_brain.nii
+mri_binarize --i ${subject}_cat12_brain.nii --min 0.001 --o ${subject}_cat12_brain_mask.nii
+fslmaths T1w_${subject} -mul ${subject}_cat12_brain_mask.nii T1w_${subject}_cat12_brain
