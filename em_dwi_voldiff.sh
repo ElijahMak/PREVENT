aafaed19937f49939e78c8a980d89c016c319031
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-09-27 10:47:21 +0100

    a

--- em_dwi_voldiff.sh
+++ em_dwi_voldiff.sh
@@ -13,7 +13,7 @@ cd ${subject}
 mri_binarize --i dti_FA_fnirt_FMRIB58.nii --min 0.0001 --o dti_FA_fnirt_FMRIB58_mask.nii
 
 # Extract difference maps
-fslmaths $prevent/templates/MNI152_T1_1mm_brain_mask.nii.gz -sub dti_FA_fnirt_FMRIB58_mask dti_FA_fnirt_FMRIB58_mask_diff_MNI152
+fslmaths $prevent/templates/MNI152_T1_1mm_brain_mask_sup.nii.gz -sub dti_FA_fnirt_FMRIB58_mask dti_FA_fnirt_FMRIB58_mask_diff_MNI152
 
 # Threshold difference map, selecting voxels present in MNI152 but not in normalised FA map
 
