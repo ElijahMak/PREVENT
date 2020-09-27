70d70012b07af55158dcddf43fcc899d5f1540c7
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-09-27 16:30:34 +0100

    a

--- em_cat12_bet.sh
+++ em_cat12_bet.sh
@@ -6,6 +6,6 @@ subject=${1}
 
 fslmaths ${dir}/mri/p1T1w_${subject}.nii -add ${dir}/mri/p2T1w_${subject}.nii ${dir}/mri/${subject}_cat12_brain.nii
 
-mri_binarize --i ${dir}/mri/${subject}_cat12_brain.nii --min 0.001 --o ${dir}/mri/${subject}_cat12_brain_mask.nii
+mri_binarize --i ${dir}/mri/${subject}_cat12_brain.nii.gz --min 0.001 --o ${dir}/mri/${subject}_cat12_brain_mask.nii
 
 fslmaths ${dir}/T1w_${subject}.nii -mul ${dir}/mri/${subject}_cat12_brain_mask.nii ${dir}/mri/T1w_${subject}_cat12_brain.nii
