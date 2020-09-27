4076753f64dd66d641f166122719ff02eefbb3d0
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-09-27 17:08:16 +0100

    a

--- em_cat_gen_masks.sh
+++ em_cat_gen_masks.sh
@@ -1,6 +1,7 @@
 module load freesurfer/7.1.0
 
 dir="/lustre/archive/p00423/PREVENT_Elijah/CAT12"
+subject=${1}
 
 mri_binarize --i ${dir}/mri/p1T1w_${subject}.nii.gz --min 0.2 --o ${dir}/mri/p1T1w_${subject}_mask_20.nii
 
