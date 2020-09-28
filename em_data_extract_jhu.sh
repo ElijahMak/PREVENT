4dbf3b5b9903072b54765b5fe2aae6563ded37db
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-09-28 14:21:13 +0100

    a

--- em_data_extract_jhu.sh
+++ em_data_extract_jhu.sh
@@ -3,9 +3,9 @@ subject=${1}
 cd $subject
 
 # Files
-fa="dti_FA_fnirt_FMRIB58.nii.gz"
-md="dti_MD_fnirt_FMRIB58.nii.gz"
-rd="dti_RD_fnirt_FMRIB58.nii.gz"
+fa="dti_FA_fnirt_FMRIB58.nii"
+md="dti_MD_fnirt_FMRIB58.nii"
+#rd="dti_RD_fnirt_FMRIB58.nii"
 outputfile="/lustre/archive/p00423/PREVENT_Elijah/data/jhu_data.txt"
 
 echo $subject >> $outputfile
@@ -17,6 +17,6 @@ fslstats -t ${fa} -k /lustre/archive/p00423/PREVENT_Elijah/data/JHU_ROI/${i}.nii
 echo md_${i} >> $outputfile
 fslstats -t ${md} -k /lustre/archive/p00423/PREVENT_Elijah/data/${i}.nii.gz -M >> $outputfile
 
-echo ${i}_rd >> $outputfile
-fslstats -t ${rd} -k /lustre/archive/p00423/PREVENT_Elijah/data/${i}.nii.gz -M >> $outputfile
+#echo ${i}_rd >> $outputfile
+#fslstats -t ${rd} -k /lustre/archive/p00423/PREVENT_Elijah/data/${i}.nii.gz -M >> $outputfile
 done
