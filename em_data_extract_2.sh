d79ab96581f207c6e8cd1bba5346474f49754957
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-09-28 08:57:52 +0100

    a

--- em_data_extract_2.sh
+++ em_data_extract_2.sh
@@ -1,8 +1,8 @@
 
 subject=${1}
-cd $subject 
+cd $subject
 
-fa="FAtoT1_NMI.nii.gz"
+fa="fa_flirt_cat12brain.nii.nii.gz"
 gm_mask="p1T1w_${subject}_mask_20.nii"
 wm_mask="p2T1w_${subject}_mask_50.nii"
 outputfile="/lustre/archive/p00423/PREVENT_Elijah/data/metrics.txt"
@@ -15,11 +15,11 @@ then
   echo "NA" >> $outputfile
   echo fa_wm >> $outputfile
   echo "NA" >> $outputfile
-else 
+else
   echo subject >> $outputfile
   echo ${subject} >> $outputfile
   echo fa_gm >> $outputfile
   fslstats -t ${fa} -k ${gm_mask} -M >> $outputfile
   echo fa_wm >> $outputfile
   fslstats -t ${fa} -k ${wm_mask} -M >> $outputfile
-  fi 
+  fi
