52cf0c69d338b4e22633930b95f82cc99e933f9d
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-09-28 09:51:21 +0100

    a

--- em_data_extract_2.sh
+++ em_data_extract_2.sh
@@ -15,7 +15,7 @@ outputfile="/lustre/archive/p00423/PREVENT_Elijah/data/metrics.txt"
 
   echo fa_gm >> $outputfile
 
-  if [ -f ${fa} -a -f ${gm_mask} ]; && then
+  if [ -f ${fa} -a -f ${gm_mask} ] && ; then
   fslstats -t ${fa} -k ${gm_mask} -M >> $outputfile
   else
     echo "NA" >> $outputfile
