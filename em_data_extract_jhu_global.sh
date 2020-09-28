962ccf3e07fc3b9ac6920613986f38b3569b5144
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-09-28 17:41:38 +0100

    a

--- em_data_extract_jhu_global.sh
+++ em_data_extract_jhu_global.sh
@@ -6,8 +6,8 @@ cd $subject
 # Files
 fa="dti_FA_fnirt_FMRIB58.nii"
 md="dti_MD_fnirt_FMRIB58.nii"
-
 #rd="dti_RD_fnirt_FMRIB58.nii"
+
 outputfile="/lustre/archive/p00423/PREVENT_Elijah/data/jhu_global_data.txt"
 
 echo subject >> $outputfile
@@ -26,5 +26,4 @@ else
       echo "NA"  >> $outputfile
       echo md_jhu_global >> $outputfile
       echo "NA"  >> $outputfile
-    done
 fi
