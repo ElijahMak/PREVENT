6703842ef829a361b2370265a189db3ee5850a54
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-09-28 09:18:41 +0100

    a

--- em_data_extract_2.sh
+++ em_data_extract_2.sh
@@ -9,35 +9,54 @@ gm_mask="p1T1w_${subject}_mask_20.nii"
 wm_mask="p2T1w_${subject}_mask_50.nii"
 outputfile="/lustre/archive/p00423/PREVENT_Elijah/data/metrics.txt"
 
-if [ `ls . | wc -l` -lt 6 ];
-then
+
   echo subject >> $outputfile
   echo ${subject} >> $outputfile
+
   echo fa_gm >> $outputfile
-  echo "NA" >> $outputfile
+
+  if [ -f ${fa} && -f ${gm_mask} ]; then
+    fslstats -t ${fa} -k ${gm_mask} -M >> $outputfile
+  else
+    echo "NA" >> $outputfile
+  fi
+
   echo fa_wm >> $outputfile
-  echo "NA" >> $outputfile
+
+  if [ -f ${fa} && -f ${wm_mask} ]; then
+    fslstats -t ${fa} -k ${wm_mask} -M >> $outputfile
+  else
+    echo "NA" >> $outputfile
+  fi
+
   echo md_gm >> $outputfile
-  echo "NA" >> $outputfile
+
+  if [ -f ${md} && -f ${gm_mask} ]; then
+    fslstats -t ${fa} -k ${gm_mask} -M >> $outputfile
+  else
+    echo "NA" >> $outputfile
+  fi
+
   echo md_wm >> $outputfile
-  echo "NA" >> $outputfile
+
+  if [ -f ${md} && -f ${wm_mask} ]; then
+    fslstats -t ${fa} -k ${wm_mask} -M >> $outputfile
+  else
+    echo "NA" >> $outputfile
+  fi
+
   echo rd_gm >> $outputfile
-  echo "NA" >> $outputfile
+
+  if [ -f ${rd} && -f ${gm_mask} ]; then
+    fslstats -t ${rd} -k ${gm_mask} -M >> $outputfile
+  else
+    echo "NA" >> $outputfile
+  fi
+
   echo rd_wm >> $outputfile
-  echo "NA" >> $outputfile
-else
-  echo subject >> $outputfile
-  echo ${subject} >> $outputfile
-  echo fa_gm >> $outputfile
-  fslstats -t ${fa} -k ${gm_mask} -M >> $outputfile
-  echo fa_wm >> $outputfile
-  fslstats -t ${fa} -k ${wm_mask} -M >> $outputfile
-  echo md_gm >> $outputfile
-  fslstats -t ${md} -k ${gm_mask} -M >> $outputfile
-  echo md_wm >> $outputfile
-  fslstats -t ${md} -k ${wm_mask} -M >> $outputfile
-  echo rd_gm >> $outputfile
-  fslstats -t ${rd} -k ${gm_mask} -M >> $outputfile
-  echo rd_wm >> $outputfile
-  fslstats -t ${rd} -k ${wm_mask} -M >> $outputfile
+
+  if [ -f ${rd} && -f ${wm_mask} ]; then
+    fslstats -t ${rd} -k ${wm_mask} -M >> $outputfile
+  else
+    echo "NA" >> $outputfile
   fi
