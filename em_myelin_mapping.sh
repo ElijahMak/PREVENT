0fb8718a571e84b6acdab6a6b3003c80716c741b
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-09-29 15:17:33 +0100

    a

--- em_myelin_mapping.sh
+++ em_myelin_mapping.sh
@@ -14,20 +14,21 @@ module load fsl/6.0.1
 
 # Parameters
 i="${1}"
-T1="T1w_${i}_BFC"
-T2="T2w_${i}_BFC"
+T1="T1w_${i}.nii"
+T2="T2w_${i}.nii"
+T1_brain="T1w_${i}_cat12_brain.nii.gz"
 omat="${i}_T2toT1_NMI.mat"
 rT2="${i}_T2toT1_NMI"
-mask="T1w_${i}_BFC_brain_mask.nii"
+mask="${i}_cat12_brain_mask.nii"
 
 # Enter directory
 cd ${i}
 
 # Linear registration
-flirt -in ${T2}.nii -ref ${T1}.nii -dof 6 -cost normmi -omat $omat -out $rT2
+flirt -in ${T2} -ref ${T1_brain} -dof 6 -cost normmi -omat $omat -out $rT2
 
 # Derive myelin map
-fslmaths ${T1}.nii -div $rT2 ${i}_myelin
+fslmaths ${T1} -div ${rT2} ${i}_myelin
 
 # Derive myelin brain map
 fslmaths ${i}_myelin -mul ${mask} ${i}_myelin_brain
