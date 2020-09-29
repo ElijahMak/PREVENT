e4569d01058de696fa38831d6c79b3567f9c11d4
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-09-29 22:49:13 +0100

    a

--- em_data_extract_jhu.sh
+++ em_data_extract_jhu.sh
@@ -7,10 +7,11 @@ subject=${1}
 # Files
 fa="/lustre/archive/p00423/PREVENT_Elijah/dwi_denoised/${subject}/dti_FA_fnirt_FMRIB58.nii"
 md="/lustre/archive/p00423/PREVENT_Elijah/dwi_denoised/${subject}/dti_MD_fnirt_FMRIB58.nii"
-rd="/lustre/archive/p00423/PREVENT_Elijah/dwi_denoised/${subject}/dti_RD_fnirt_FMRIB58.nii"
+odi="/lustre/archive/p00423/PREVENT_Elijah/dwi_denoised/${subject}/dti_ODI_fnirt_FMRIB58.nii"
+fw="/lustre/archive/p00423/PREVENT_Elijah/dwi_denoised/${subject}/dti_FW_fnirt_FMRIB58.nii"
 
 # Output
-outputfile="/lustre/archive/p00423/PREVENT_Elijah/data/${subject}_dwi_fnirt_jhu_roi.txt"
+outputfile="/lustre/archive/p00423/PREVENT_Elijah/data/${subject}_NODDI_fnirt_jhu_roi.txt"
 
 rm $outputfile
 
@@ -19,12 +20,18 @@ rm $outputfile
 echo subject >> $outputfile
 echo $subject >> $outputfile
 
+if [ ${measure} = "DTI" ]; then
 
 # Loop through JHU ROIs if FA file is present
 # ------------------------------------------------------------------------
 if test -a ${fa};
+
 then
-  for i in `cat /lustre/archive/p00423/PREVENT_Elijah/data/roi`; do
+
+  for i in `cat /lustre/archive/p00423/PREVENT_Elijah/data/roi`;
+
+  do
+
   echo dwi_fa_${i} >> $outputfile
   fslstats -t ${fa} -k /lustre/archive/p00423/PREVENT_Elijah/data/JHU_ROI/${i}.nii.gz -M >> $outputfile
 
@@ -33,17 +40,70 @@ then
 
   echo dwi_rd_${i} >> $outputfile
   fslstats -t ${rd} -k /lustre/archive/p00423/PREVENT_Elijah/data/JHU_ROI/${i}.nii.gz -M >> $outputfile
+
   done
+
+else
 
 #  Print NA if FA is missing for each ROI
 # ------------------------------------------------------------------------
-else
-    for i in `cat /lustre/archive/p00423/PREVENT_Elijah/data/roi`; do
+
+    for i in `cat /lustre/archive/p00423/PREVENT_Elijah/data/roi`;
+
+    do
+
       echo dwi_fa_${i} >> $outputfile
       echo "NA"  >> $outputfile
+
       echo dwi_md_${i} >> $outputfile
       echo "NA"  >> $outputfile
+
       echo dwi_rd_${i} >> $outputfile
       echo "NA"  >> $outputfile
+
     done
+
+  fi
+
+  elif  [ ${measure} = "NODDI" ];
+
+  then
+
+# Loop through JHU ROIs if ODI file is present
+# ------------------------------------------------------------------------
+
+    if test -a ${odi};
+
+    then
+
+      for i in `cat /lustre/archive/p00423/PREVENT_Elijah/data/roi`;
+
+      do
+
+        echo dwi_ODI_${i} >> $outputfile
+
+        fslstats -t ${odi} -k /lustre/archive/p00423/PREVENT_Elijah/data/JHU_ROI/${i}.nii.gz -M >> $outputfile
+
+        echo dwi_FW_${i} >> $outputfile
+        fslstats -t ${fw} -k /lustre/archive/p00423/PREVENT_Elijah/data/JHU_ROI/${i}.nii.gz -M >> $outputfile
+
+      done
+
+#  Print NA if ODI is missing for each ROI
+# ------------------------------------------------------------------------
+    else
+
+        for i in `cat /lustre/archive/p00423/PREVENT_Elijah/data/roi`;
+
+        do
+          echo dwi_ODI_${i} >> $outputfile
+          echo "NA"  >> $outputfile
+
+          echo dwi_FW_${i} >> $outputfile
+          echo "NA"  >> $outputfile
+
+        done
+
+      fi
+
 fi
