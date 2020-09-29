d108fd822244d6b79ef97a980123ce188937f5c0
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-09-29 22:38:45 +0100

    a

--- em_data_extract_NODDI_flirt.sh
+++ em_data_extract_NODDI_flirt.sh
@@ -3,9 +3,11 @@ subject=${1}
 
 # Files
 ODI="/lustre/archive/p00423/PREVENT_Elijah/NODDI/${subject}/odi_flirt_cat12brain.nii.gz"
+FW="/lustre/archive/p00423/PREVENT_Elijah/NODDI/${subject}/fw_flirt_cat12brain.nii.gz"
+
 gm_mask="/lustre/archive/p00423/PREVENT_Elijah/CAT12/mri/p1T1w_${subject}_mask_20.nii"
 wm_mask="/lustre/archive/p00423/PREVENT_Elijah/CAT12/mri/p2T1w_${subject}_mask_90.nii"
-outputfile="/lustre/archive/p00423/PREVENT_Elijah/data/${subject}_ODI_flirt.txt"
+outputfile="/lustre/archive/p00423/PREVENT_Elijah/data/${subject}_NODDI_flirt.txt"
 rm $outputfile
 
 # ODI GM
@@ -21,12 +23,33 @@ if test -a ${ODI} && test -a ${gm_mask};
     echo "NA"  >> $outputfile
   fi
 
-# Myelin WM
+# ODI WM
 # --------------------------------------------------------------------
   echo ODI_wm >> $outputfile
 
   if test -a ${ODI} && test -a ${wm_mask}; then
     fslstats -t ${ODI} -k ${wm_mask} -M >> $outputfile
+  else
+    echo "NA" >> $outputfile
+  fi
+
+# FW GM
+# --------------------------------------------------------------------
+  echo FW_gm >> $outputfile
+
+  if test -a ${FW} && test -a ${gm_mask}; then
+    fslstats -t ${FW} -k ${gm_mask} -M >> $outputfile
+  else
+    echo "NA" >> $outputfile
+  fi
+
+
+# FW WM
+# --------------------------------------------------------------------
+  echo FW_wm >> $outputfile
+
+  if test -a ${FW} && test -a ${wm_mask}; then
+    fslstats -t ${FW} -k ${wm_mask} -M >> $outputfile
   else
     echo "NA" >> $outputfile
   fi
