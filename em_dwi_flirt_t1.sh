45660a2ce961deb63d91d594b78e538e10831b3f
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-09-29 18:36:01 +0100

    a

--- em_dwi_flirt_t1.sh
+++ em_dwi_flirt_t1.sh
@@ -19,13 +19,18 @@ md="/lustre/archive/p00423/PREVENT_Elijah/dwi_denoised/${subject}/dti_MD.nii"
 
 rd="/lustre/archive/p00423/PREVENT_Elijah/dwi_denoised/${subject}/dti_RD.nii"
 
+odi="/lustre/archive/p00423/PREVENT_Elijah/NODDI/${subject}/ODI.nii.gz"
+
 t1_brain="/lustre/archive/p00423/PREVENT_Elijah/CAT12/mri/T1w_${subject}_cat12_brain.nii.gz"
 
 # Register FA to T1
-flirt -in ${fa} -ref ${t1_brain} -dof 6 -cost normmi -omat fa_flirt_cat12brain.mat -out fa_flirt_cat12brain
+#flirt -in ${fa} -ref ${t1_brain} -dof 6 -cost normmi -omat fa_flirt_cat12brain.mat -out fa_flirt_cat12brain
 
 # Warp MD to T1
-applywarp -i ${md} -o md_flirt_cat12brain --premat=fa_flirt_cat12brain.mat -r ${t1_brain}
+#applywarp -i ${md} -o md_flirt_cat12brain --premat=/lustre/archive/p00423/PREVENT_Elijah/dwi_denoised/${subject}/fa_flirt_cat12brain.mat -r ${t1_brain}
 
 # Warp RD to T1
-applywarp -i dti_RD.nii -o rd_flirt_cat12brain --premat=fa_flirt_cat12brain.mat -r ${t1_brain}
+#applywarp -i dti_RD.nii -o rd_flirt_cat12brain --premat=/lustre/archive/p00423/PREVENT_Elijah/dwi_denoised/${subject}/fa_flirt_cat12brain.mat -r ${t1_brain}
+
+# Warp ODI to T1
+applywarp -i ${odi} -o odi_flirt_cat12brain --premat=/lustre/archive/p00423/PREVENT_Elijah/dwi_denoised/${subject}/fa_flirt_cat12brain.mat -r ${t1_brain}
