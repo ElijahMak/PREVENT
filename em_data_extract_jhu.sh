9cea5ee86f359d6d9c69148495c9ceb33b293c02
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-09-29 14:03:45 +0100

    a

--- em_data_extract_jhu.sh
+++ em_data_extract_jhu.sh
@@ -4,10 +4,15 @@ subject=${1}
 cd $subject
 
 # Files
-fa="dti_FA_fnirt_FMRIB58.nii"
-md="dti_MD_fnirt_FMRIB58.nii"
-#rd="dti_RD_fnirt_FMRIB58.nii"
-outputfile="/lustre/archive/p00423/PREVENT_Elijah/data/jhu_data_2.txt"
+# Files
+fa="/lustre/archive/p00423/PREVENT_Elijah/dwi_denoised/${subject}/dti_FA_fnirt_FMRIB58.nii.gz"
+md="/lustre/archive/p00423/PREVENT_Elijah/dwi_denoised/${subject}/dti_MD_fnirt_FMRIB58.nii.gz"
+rd="/lustre/archive/p00423/PREVENT_Elijah/dwi_denoised/${subject}/dti_RD_fnirt_FMRIB58.nii.gz"
+
+outputfile="/lustre/archive/p00423/PREVENT_Elijah/data/${subject}_dwi_fnirt_jhu_roi.txt"
+
+rm $outputfile
+
 
 echo subject >> $outputfile
 echo $subject >> $outputfile
@@ -21,14 +26,17 @@ then
   echo md_${i} >> $outputfile
   fslstats -t ${md} -k /lustre/archive/p00423/PREVENT_Elijah/data/JHU_ROI/${i}.nii.gz -M >> $outputfile
 
-  #echo ${i}_rd >> $outputfile
-  #fslstats -t ${rd} -k /lustre/archive/p00423/PREVENT_Elijah/data/${i}.nii.gz -M >> $outputfile
+  echo ${i}_rd >> $outputfile
+  fslstats -t ${rd} -k /lustre/archive/p00423/PREVENT_Elijah/data/${i}.nii.gz -M >> $outputfile
   done
+
 else
     for i in `cat /lustre/archive/p00423/PREVENT_Elijah/data/roi`; do
       echo fa_${i} >> $outputfile
       echo "NA"  >> $outputfile
       echo md_${i} >> $outputfile
+      echo "NA"  >> $outputfile
+      echo rd_${i} >> $outputfile
       echo "NA"  >> $outputfile
     done
 fi
