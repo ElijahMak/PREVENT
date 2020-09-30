cba4c55f3dbdf0f383537c0eb4e8621b36c7dd30
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-09-30 16:36:15 +0100

    a

--- em_data_compile_jhu_full.sh
+++ em_data_compile_jhu_full.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-in_file=all_NODDI_native_jhu_roi.txt      # Input file
-params=49       # Parameters count
+in_file=${1}      # Input file
+params=142       # Parameters count
 res_file=$(mktemp)  # Temporary file
 sep=' '             # Separator character
 
