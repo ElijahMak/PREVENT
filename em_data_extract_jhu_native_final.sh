197e456d81301a267034cdd7e9fd9cb7273c342b
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-10-01 13:01:02 +0100

    a

--- em_data_extract_jhu_native_final.sh
+++ em_data_extract_jhu_native_final.sh
@@ -5,25 +5,33 @@ subject=${1}
 cd $subject
 
 for dti in FA MD RD; do
-  for roinum in {1..47} ; do
 
-    file="/lustre/archive/p00423/PREVENT_Elijah/dwi_denoised/${subject}/dti_${dti}.nii"
+  file="/lustre/archive/p00423/PREVENT_Elijah/dwi_denoised/${subject}/dti_${dti}.nii"
 
-     padroi=`$FSLDIR/bin/zeropad $roinum 3`
+  if [-f ${file} ]; then
 
-    if [ -f ${subject}_${dti}_jhu_native_roi${padroi}.txt ]
+    for roinum in {1..47} ; do
 
-    then
-         rm ${subject}_${dti}_jhu_native_roi${padroi}.txt
-    fi
+      padroi=`$FSLDIR/bin/zeropad $roinum 3`
 
-    #echo $(sed "${roinum}q;d" /lustre/archive/p00423/PREVENT_Elijah/dwi_denoised/list_jhu) >> ${subject}_${dti}_jhu_native_roi${padroi}.txt
+      if [ -f ${subject}_${dti}_jhu_native_roi${padroi}.txt ]; then
+         rm ${subject}_${dti}_jhu_native_roi${padroi}.txt; fi
 
-     #echo ${roinum} >> ${subject}_${dti}_jhu_native_roi${padroi}.txt
+          #echo $(sed "${roinum}q;d" /lustre/archive/p00423/PREVENT_Elijah/dwi_denoised/list_jhu) >> ${subject}_${dti}_jhu_native_roi${padroi}.txt
 
-     fslmeants -i ${file} -m warped_jhu/${roinum} >> ${subject}_${dti}_jhu_native_roi${padroi}.txt
+          #echo ${roinum} >> ${subject}_${dti}_jhu_native_roi${padroi}.txt
 
-     paste *${dti}_jhu_native*.txt > all_${dti}_jhu.txt
+          fslmeants -i ${file} -m warped_jhu/${roinum} >> ${subject}_${dti}_jhu_native_roi${padroi}.txt
+
+          paste *${dti}_jhu_native*.txt > all_${dti}_jhu.txt
+
+        else
+
+          echo "NA" >> ${subject}_${dti}_jhu_native_roi${padroi}.txt
+
+          paste *${dti}_jhu_native*.txt > all_${dti}_jhu.txt
+
+        fi
 
   done
 
