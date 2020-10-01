c4dc04323bcdf7ba0926a4a86ef46a0231631661
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-10-01 12:18:29 +0100

    a

--- em_data_extract_jhu_native_final.sh
+++ em_data_extract_jhu_native_final.sh
@@ -11,7 +11,11 @@ for dti in FA MD RD; do
 
      padroi=`$FSLDIR/bin/zeropad $roinum 3`
 
-     rm ${subject}_${dti}_jhu_native_roi${padroi}.txt
+    if [ -f ${subject}_${dti}_jhu_native_roi${padroi}.txt ]
+
+    then
+         rm ${subject}_${dti}_jhu_native_roi${padroi}.txt
+    fi
 
      sed "${roinum}q;d" /lustre/archive/p00423/PREVENT_Elijah/dwi_denoised/list_jhu >> ${subject}_${dti}_jhu_native_roi${padroi}.txt
 
