22727743ce434a2f9d8d76adc65fe0acf716a225
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-10-01 12:15:25 +0100

    a

--- em_data_extract_jhu_native_final.sh
+++ em_data_extract_jhu_native_final.sh
@@ -4,9 +4,8 @@ subject=${1}
 
 cd $subject
 
-for roinum in {1..47} ; do
-
-  for dti in FA MD RD; do
+for dti in FA MD RD; do
+  for roinum in {1..47} ; do
 
     file="/lustre/archive/p00423/PREVENT_Elijah/dwi_denoised/${subject}/dti_${dti}.nii"
 
@@ -16,5 +15,8 @@ for roinum in {1..47} ; do
      sed "${roinum}q;d" /lustre/archive/p00423/PREVENT_Elijah/dwi_denoised/list_jhu >> ${subject}_${dti}_jhu_native_roi${padroi}.txt
      fslmeants -i ${file} -m warped_jhu/${roinum} >> ${subject}_${dti}_jhu_native_roi${padroi}.txt
 
+
+     paste *${dti}_jhu_native*.txt > all_${dti}_jhu.txt
+     rm *${dti}_jhu_native_roi*.txt
   done
 done
