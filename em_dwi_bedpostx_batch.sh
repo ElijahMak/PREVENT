9a8972d5c464b95c85ad73346a839ff4839272ce
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-10-07 12:44:27 +0100

    a

---
+++ em_dwi_bedpostx_batch.sh
@@ -0,0 +1,30 @@
+# Job submission for BEDPOSTX preprocessing on WBIC
+
+#!/bin/bash
+
+# Author: Elijah Mak
+# Date: 22nd September 2020
+
+# Estimated time on HPHI is 15 hours
+
+# Time
+time="17:00:00"
+
+# Memory
+mem="4000"
+
+# Number of tasks
+nTask=1
+
+# List of subjects to process
+input=${1}
+
+# Execute job submission
+for subject in `cat $input`; do
+
+sbatch --job-name=bedpostx --account hphi --qos=day.q --partition wbic-cs --error=${subject%.*}_%j.err \
+--output=${subject%.*}_%j.out --time=${time} --nodes=1 --ntasks-per-node=${nTask} --mem=${mem} \
+--wrap="module unload fsl/5.0.10; module load fsl/6.0.3; bedpostx ${subject}" --mail-type=ALL
+
+sleep 1
+done
