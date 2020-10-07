38b36cb0263d362727ceb0e4848712cf5db65390
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-10-07 12:22:21 +0100

    a

--- em_dwi_bedpostx_prep.sh
+++ em_dwi_bedpostx_prep.sh
@@ -1,9 +1,9 @@
 # Prepare images for BEDPOSTX
 cd $prevent/dwi_denoised
 i=${1}
-cp ${i}/dwi.denoised.nii BEDPOSTX/${i}/data.nii
-cp ${i}/bval BEDPOSTX/${i}/bvals
-cp ${i}/bvec BEDPOSTX/${i}/bvecs
-cp ${i}/b0_brain_mask.nii.gz BEDPOSTX/${i}/nodif_brain_mask.nii.gz
-cp ${i}/acqparams.txt BEDPOSTX/${i}/acqparams.txt
-cp ${i}/index.txt BEDPOSTX/${i}/index.txt
+cp ${i}/dwi.denoised.nii $prevent/BEDPOSTX/${i}/data.nii
+cp ${i}/bval $prevent/BEDPOSTX/${i}/bvals
+cp ${i}/bvec $prevent/BEDPOSTX/${i}/bvecs
+cp ${i}/b0_brain_mask.nii.gz $prevent/BEDPOSTX/${i}/nodif_brain_mask.nii.gz
+cp ${i}/acqparams.txt $prevent/BEDPOSTX/${i}/acqparams.txt
+cp ${i}/index.txt $prevent/BEDPOSTX/${i}/index.txt
