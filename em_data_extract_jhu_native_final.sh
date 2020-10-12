74e79588e605e8caa8196e9d0cb6b59873e7e9e7
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-10-12 11:16:22 +0100

    a

--- em_data_extract_jhu_native_final.sh
+++ em_data_extract_jhu_native_final.sh
@@ -1,42 +1,40 @@
 
 subject=${1}
-
+dti=${2}
 cd $subject
 
-for dti in FA MD RD; do
+file="/lustre/archive/p00423/PREVENT_Elijah/dwi_denoised/${subject}/dti_${dti}.nii"
 
-  file="/lustre/archive/p00423/PREVENT_Elijah/dwi_denoised/${subject}/dti_${dti}.nii"
+if [ -f ${file} ]; then
 
-  if [ -f ${file} ]; then
+  for roinum in {1..48} ; do
+
+    padroi=`$FSLDIR/bin/zeropad $roinum 3`
+
+    if [ -f ${subject}_${dti}_jhu_native_roi${padroi}.txt ]; then
+      rm ${subject}_${dti}_jhu_native_roi${padroi}.txt; fi
+
+      #echo $(sed "${roinum}q;d" /lustre/archive/p00423/PREVENT_Elijah/dwi_denoised/list_jhu) >> ${subject}_${dti}_jhu_native_roi${padroi}.txt
+
+      #echo ${roinum} >> ${subject}_${dti}_jhu_native_roi${padroi}.txt
+
+      fslmeants -i ${file} -m warped_jhu/${roinum}.nii >> ${subject}_${dti}_jhu_native_roi${padroi}.txt
+
+      paste *${dti}_jhu_native*.txt > all_${dti}_jhu.txt
+
+    done
+
+  else
 
     for roinum in {1..48} ; do
 
       padroi=`$FSLDIR/bin/zeropad $roinum 3`
 
       if [ -f ${subject}_${dti}_jhu_native_roi${padroi}.txt ]; then
-         rm ${subject}_${dti}_jhu_native_roi${padroi}.txt; fi
+        rm ${subject}_${dti}_jhu_native_roi${padroi}.txt; fi
 
-          #echo $(sed "${roinum}q;d" /lustre/archive/p00423/PREVENT_Elijah/dwi_denoised/list_jhu) >> ${subject}_${dti}_jhu_native_roi${padroi}.txt
-
-          #echo ${roinum} >> ${subject}_${dti}_jhu_native_roi${padroi}.txt
-
-          fslmeants -i ${file} -m warped_jhu/${roinum}.nii >> ${subject}_${dti}_jhu_native_roi${padroi}.txt
-
-          paste *${dti}_jhu_native*.txt > all_${dti}_jhu.txt
-
-        done
-
-        else
-
-          for roinum in {1..48} ; do
-
-            padroi=`$FSLDIR/bin/zeropad $roinum 3`
-
-            if [ -f ${subject}_${dti}_jhu_native_roi${padroi}.txt ]; then
-               rm ${subject}_${dti}_jhu_native_roi${padroi}.txt; fi
-
-          echo "NA" >> ${subject}_${dti}_jhu_native_roi${padroi}.txt
-          paste *${dti}_jhu_native*.txt > all_${dti}_jhu.txt
-        done
-      fi
+        echo "NA" >> ${subject}_${dti}_jhu_native_roi${padroi}.txt
+        paste *${dti}_jhu_native*.txt > all_${dti}_jhu.txt
       done
+    fi
+    
