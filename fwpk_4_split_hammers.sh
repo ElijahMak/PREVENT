685788ad261a10a98ae73d63a7b79d0be7a91db5
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-10-15 05:06:12 +0100

    a

--- fwpk_4_split_hammers.sh
+++ fwpk_4_split_hammers.sh
@@ -13,12 +13,12 @@ done
 #       do fslmaths hammers/w${subject}_Hammers_mith_icbm_psp_gm_mask_50.nii.gz -thr ${i} -uthr ${i} hammers/${i}_gm_mask_50.nii.gz
 # done
 
-# 50 WM
-for i in `cat ../index`
-      do fslmaths hammers/w${subject}_Hammers_mith_icbm_psp_wm_mask_50.nii.gz -thr ${i} -uthr ${i} hammers/${i}_wm_mask_50.nii.gz
-done
-
 # 20 GM
 for i in `cat ../index`
       do fslmaths hammers/w${subject}_Hammers_mith_icbm_psp_gm_mask_20.nii.gz -thr ${i} -uthr ${i} hammers/${i}_gm_mask_20.nii.gz
+done
+
+# 50 WM
+for i in `cat ../index`
+      do fslmaths hammers/w${subject}_Hammers_mith_icbm_psp_wm_mask_50.nii.gz -thr ${i} -uthr ${i} hammers/${i}_wm_mask_50.nii.gz
 done
