cf6d05191aa60c6295a97e86e2f1a55e6a55e8a8
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-10-15 04:59:48 +0100

    a

--- fwpk_4_split_hammers.sh
+++ fwpk_4_split_hammers.sh
@@ -3,18 +3,22 @@ subject=${1}
 
 cd $subject
 
-
 # Full hammers
 for i in `cat ../index`
       do fslmaths hammers/w${subject}_Hammers_mith_icbm_psp.nii.gz -thr ${i} -uthr ${i} hammers/${i}.nii.gz
 done
 
-# 50 GM
-for i in `cat ../index`
-      do fslmaths hammers/w${subject}_Hammers_mith_icbm_psp_gm_mask_50.nii.gz -thr ${i} -uthr ${i} hammers/${i}_gm_mask_50.nii.gz
-done
+# # 50 GM
+# for i in `cat ../index`
+#       do fslmaths hammers/w${subject}_Hammers_mith_icbm_psp_gm_mask_50.nii.gz -thr ${i} -uthr ${i} hammers/${i}_gm_mask_50.nii.gz
+# done
 
 # 50 WM
 for i in `cat ../index`
-      do fslmaths hammers/w${subject}_Hammers_mith_icbm_psp_gm_mask_50.nii.gz -thr ${i} -uthr ${i} hammers/${i}_wm_mask_50.nii.gz
+      do fslmaths hammers/w${subject}_Hammers_mith_icbm_psp_wm_mask_50.nii.gz -thr ${i} -uthr ${i} hammers/${i}_wm_mask_50.nii.gz
+done
+
+# 20 GM
+for i in `cat ../index`
+      do fslmaths hammers/w${subject}_Hammers_mith_icbm_psp_gm_mask_20.nii.gz -thr ${i} -uthr ${i} hammers/${i}_gm_mask_20.nii.gz
 done
