82fb22151321acdb17d5028bc523332dda8c3678
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-10-23 13:01:58 +0100

    a

--- em_mrtrix.sh
+++ em_mrtrix.sh
@@ -1,45 +1,121 @@
 # MRTRIX pipeline
 
 # To generate connectomes from eddy-corrected data.
+# Author: Elijah Mak
 
 # Modules
+# -----------------------------------------------------------------------------
+
 module load freesurfer/7.1.0
-module load MRtrix/mrtrix-3.0.2
-module load ANTS/2.2.0
+mrmodule load ANTS/2.2.0
+module unload fsl/5.0.10
+module load fsl/6.0.3
 
-# Subject
+# Parameters
+# -----------------------------------------------------------------------------
+
+dir="/lustre/archive/p00423/PREVENT_Elijah/MRTRIX/AFD"
 subject=${1}
-cd $subject
+BET=${2}
+raw_dwi="dwi.nii"
+denoised_dwi="dwi.denoised.nii"
+denoised_degibbs_dwi="dwi.denoised.degibbs.nii"
+b0="denoised_degibbs_dwi_b0.nii.gz"
+mask="denoised_degibbs_dwi_b0_brain_f02_mask.nii.gz"
+output_edc="denoised_degibbs.edc.repol"
+edc="denoised_degibbs.edc.repol.nii.gz"
+edc_rotated_bvecs="denoised_degibbs.edc.repol.eddy_rotated_bvecs"
+edc_mif="denoised_degibbs.edc.repol.mif"
+edc_mif_bfc="denoised_degibbs.edc.repol.bfc.mif"
+edc_mif_bfc_tensor="denoised_degibbs.edc.repol.bfc.tensor.mif"
+edc_mif_bfc_tensor_FA="denoised_degibbs.edc.repol.bfc.tensor.FA.mif"
+edc_mif_bfc_tensor_MD="denoised_degibbs.edc.repol.bfc.tensor.MD.mif"
+edc_mif_bfc_tensor_RD="denoised_degibbs.edc.repol.bfc.tensor.RD.mif"
+edc_mif_bfc_tensor_AD="denoised_degibbs.edc.repol.bfc.tensor.AD.mif"
+edc_nii_bfc_tensor_FA="denoised_degibbs.edc.repol.bfc.tensor.FA.nii"
+edc_nii_bfc_tensor_MD="denoised_degibbs.edc.repol.bfc.tensor.MD.nii"
+edc_nii_bfc_tensor_RD="denoised_degibbs.edc.repol.bfc.tensor.RD.nii"
+edc_nii_bfc_tensor_AD="denoised_degibbs.edc.repol.bfc.tensor.AD.nii"
 
-# Convert the diffusion images into a non-compressed format (not strictly necessary, but will make subsequent processing faster), embed the diffusion gradient encoding information within the image header, re-arrange the data strides to make volume data contiguous in memory for each voxel, and convert to floating-point representation (makes data access faster in subsequent commands):
 
-mrconvert edc.repol.nii edc.repol.mif -fslgrad edc.repol.eddy_rotated_bvecs bval -datatype float32 -strides 0,0,0,1
+# Preprocessing
+# -----------------------------------------------------------------------------
 
-dwibiascorrect ants -mask b0_brain_mask.nii.gz -bias bias.nii edc.repol.mif edc.repol.bfc.mif
+cd ${dir}
+cd ${subject}
 
-dwi2tensor -mask b0_brain_mask.nii.gz edc.repol.bfc.mif tensor.mif
+# Denoising of DWI
+dwidenoise ${raw_dwi} ${denoised_dwi}
 
-tensor2metric -fa FA.mif tensor.mif -force
-tensor2metric -adc MD.mif tensor.mif -force
-tensor2metric -ad AD.mif tensor.mif -force
-tensor2metric -rd RD.mif tensor.mif -force
+# Remove Gibbs Ringing artifact
+mrdegibbs ${denoised_dwi} ${denoised_degibbs_dwi}
 
-dwi2response tournier edc.repol.bfc.mif wm_response.txt -voxels voxels.mif -mask b0_brain_mask.nii.gz
+# Perform brain extraction
+if [ "${BET}" = "0" ]; then
+  echo "Running BET [1/3]"
+  echo "fslroi $denoised_degibbs_dwi $b0 0 1"
+  fslroi ${denoised_degibbs_dwi} ${b0} 0 1
+  echo "bet b0 b0_brain -m -f 0.2"
+  bet ${b0} denoised_degibbs_dwi_b0_brain_f01 -m -f 0.1
+  bet ${b0} denoised_degibbs_dwi_b0_brain_f02 -m -f 0.2
+  bet ${b0} denoised_degibbs_dwi_b0_brain_f03 -m -f 0.3
+  bet ${b0} denoised_degibbs_dwi_b0_brain_f04 -m -f 0.4
+else
+  echo "Using edited BET [1/3]"
+fi
 
-dwi2fod csd edc.repol.mif wm_response.txt fod.mif -mask b0_brain_mask.nii.gz
+# Eddy current correction
+eddy_openmp --imain=${denoised_degibbs_dwi} \
+--mask=${mask} --acqp=acqparams.txt \
+--index=index.txt --bvecs=bvec \
+--bvals=bval --fwhm=10,0,0,0,0 \
+--repol \
+--out=$output_edc \
+--cnr_maps \
+--verbose
 
-# The resulting WM fODFs can be displayed together with the mean fODF amplitude map using:
-# mrview fod.mif -odf.load_sh fod.mif
+
+# Bias-field correction and tensor fitting
+# -----------------------------------------------------------------------------
+
+# Convert to mif
+mrconvert ${edc} ${edc_mif} -fslgrad ${output_edc}.eddy_rotated_bvecs bval -datatype float32 -strides 0,0,0,1
+
+# Perform bias-field correction on
+dwibiascorrect ants -mask ${mask} -bias bias.nii ${edc_mif} ${edc_mif_bfc}
+
+# Extract tensor maps
+dwi2tensor -mask ${mask} ${edc_mif_bfc} ${edc_mif_bfc_tensor}
+
+# Fit tensor maps
+tensor2metric -fa ${edc_mif_bfc_tensor_FA} ${edc_mif_bfc_tensor} -force
+tensor2metric -adc ${edc_mif_bfc_tensor_MD} ${edc_mif_bfc_tensor} -force
+tensor2metric -ad ${edc_mif_bfc_tensor_AD} ${edc_mif_bfc_tensor} -force
+tensor2metric -rd ${edc_mif_bfc_tensor_RD} ${edc_mif_bfc_tensor} -force
+mrconvert ${edc_mif_bfc_tensor_FA} ${edc_nii_bfc_tensor_FA}
+mrconvert ${edc_mif_bfc_tensor_MD} ${edc_nii_bfc_tensor_MD}
+mrconvert ${edc_mif_bfc_tensor_AD} ${edc_nii_bfc_tensor_AD}
+mrconvert ${edc_mif_bfc_tensor_RD} ${edc_nii_bfc_tensor_RD}
+
+# Estimate response for FOD
+dwi2response tournier ${edc_mif_bfc} tournier_response.txt -voxels tournier_voxels.mif -mask ${mask}
+
+# Perform CSD
+dwi2fod csd ${edc_mif_bfc} tournier_response.txt tournier_response_fod.mif -mask ${mask}
+
+
+# Connectome pipeline
+# -----------------------------------------------------------------------------
 
 # Freesurfer registration
 export SUBJECTS_DIR=/lustre/archive/p00423/PREVENT_Elijah/Freesurfer7_GS
 
-mri_coreg --s ${subject} --mov /lustre/archive/p00423/PREVENT_Elijah/dwi_denoised/${subject}/b0.nii.gz --reg $SUBJECTS_DIR/${subject}/b0.coreg.lta
+mri_coreg --s ${subject} --mov ${b0} --reg $SUBJECTS_DIR/${subject}/b0.coreg.lta
 
 # Inverse warp segmentations into DTI displaySpace
-mri_vol2vol --mov b0.nii.gz --targ $SUBJECTS_DIR/${subject}/mri/brainmask.mgz --inv --interp nearest --o rbrainmask.nii --reg $SUBJECTS_DIR/${subject}/b0.coreg.lta
+mri_vol2vol --mov ${b0} --targ $SUBJECTS_DIR/${subject}/mri/brainmask.mgz --inv --interp nearest --o rbrainmask.nii --reg $SUBJECTS_DIR/${subject}/b0.coreg.lta
 
-mri_vol2vol --mov b0.nii.gz --targ $SUBJECTS_DIR/${subject}/mri/aparc+aseg.mgz --inv --interp nearest --o raparcaseg.nii --reg $SUBJECTS_DIR/${subject}/b0.coreg.lta
+mri_vol2vol --mov ${b0} --targ $SUBJECTS_DIR/${subject}/mri/aparc+aseg.mgz --inv --interp nearest --o raparcaseg.nii --reg $SUBJECTS_DIR/${subject}/b0.coreg.lta
 
 5ttgen freesurfer raparc+aseg.nii 5ttseg.mif -sgm_amyg_hipp -lut $FREESURFER_HOME/FreeSurferColorLUT.txt -lut $FREESURFER_HOME/FreeSurferColorLUT.txt
 
@@ -56,3 +132,94 @@ labelconvert raparc+aseg.nii $FREESURFER_HOME/FreeSurferColorLUT.txt $code/fs_de
 tck2connectome -assignment_radial_search 2 -scale_length -out_assignments assignments2.txt -tck_weights_in wholebrain_sift2_weights.txt wholebrain.tck output_parcels.mif connectome.csv -force
 
 connectome2tck wholebrain.tck assignments2.txt exemplars.tck -files single -exemplars output_parcels.mif
+
+
+# Extract ROI from JHU ICBM atlas
+# -----------------------------------------------------------------------------
+
+# Normalisation to MNI152
+
+# Parameters
+FMRIB58_FA_1mm="/lustre/archive/p00423/PREVENT_Elijah/FAST/FMRIB58_FA_1mm.nii.gz"
+flirt_out="denoised_degibbs.edc.repol.bfc.tensor.FA.flirt.nii"
+flirt_omat="denoised_degibbs.edc.repol.bfc.tensor.FA.flirt.mat"
+bins="256"
+cost="corratio"
+dof="12"
+interp="trilinear"
+fnirt_omat="denoised_degibbs.edc.repol.bfc.tensor.FA.fnirt.mat"
+FA_fnirt_out="denoised_degibbs.edc.repol.bfc.tensor.FA.fnirt.nii"
+MD_fnirt_out="denoised_degibbs.edc.repol.bfc.tensor.MD.fnirt.nii"
+RD_fnirt_out="denoised_degibbs.edc.repol.bfc.tensor.RD.fnirt.nii"
+AD_fnirt_out="denoised_degibbs.edc.repol.bfc.tensor.AD.fnirt.nii"
+
+echo "Started: Normalistion to FMRIB using FA ${subject}"
+
+# FLIRT
+flirt -in ${edc_nii_bfc_tensor_FA} -ref ${FMRIB58_FA_1mm} -out ${flirt_omat} -omat ${omat} -bins ${bins} -cost ${cost} \
+-searchrx -90 90 -searchry -90 90 -searchrz -90 90 -dof ${dof} -interp ${interp}
+# FNIRT
+fnirt --ref=${ref} --in=${FA} --aff=${flirt_omat} \
+--cout=${fnirt_omat} --config=FA_2_FMRIB58_1mm
+
+# Apply warps
+applywarp --ref=${ref} --in=${FA} --warp=${fnirt_omat} \
+--out=${FA_fnirt_out}
+applywarp --ref=${ref} --in=${MD} --warp=${fnirt_omat} \
+--out=${MD_fnirt_out}
+applywarp --ref=${ref} --in=${RD} --warp=${fnirt_omat} \
+--out=${RD_fnirt_out}
+applywarp --ref=${ref} --in=${AD} --warp=${fnirt_omat} \
+--out=${AD_fnirt_out}
+
+echo "Completed: Normalistion to FMRIB using FA ${subject}"
+
+# Inverse transform JHU labels from FMRIB space
+echo "Started: Inverse warp JHU labels ${subject}"
+
+mkdir JHU
+
+invwarp --warp=${FA_fnirt_out}\
+--ref=${FA} \
+--out=invwarp_FMRIB_to_FA
+
+for i in {1..48}
+do
+  applywarp -i /lustre/archive/p00423/PREVENT_Elijah/data/em_jhu_toolbox/${i}.nii.gz \
+  -o JHU/${i}.nii.gz \
+  -r ${FA} \
+  -w invwarp_FMRIB_to_FA.nii.gz \
+  --interp=nn
+done
+
+echo "Completed: Inverse warp JHU labels ${subject}"
+
+#  Calculate metrics of JHU labels in native space
+echo "Started: Calculating FA, MD, AD, RD in JHU labels ${subject}"
+
+for dti in FA MD RD AD; do
+  file="denoised_degibbs.edc.repol.bfc.tensor.${dti}.nii"
+  if [ -f ${file} ]; then
+    for roinum in {1..48} ; do
+      padroi=`$FSLDIR/bin/zeropad $roinum 3`
+      if [ -f JHU/${dti}_JHU_${padroi}.txt ];
+      then
+        rm JHU/${dti}_JHU_${padroi}.txt;
+      fi
+      fslmeants -i ${file} -m warped_jhu/${roinum}.nii >>
+      JHU/${dti}_JHU_${padroi}.txt
+      paste JHU/*${dti}_JHU_*.txt > JHU/all_${dti}_jhu.txt
+    done
+  else
+    for roinum in {1..48} ; do
+      padroi=`$FSLDIR/bin/zeropad $roinum 3`
+      if [ -f JHU/${dti}_JHU_${padroi}.txt ]; then
+        rm JHU/${dti}_JHU_${padroi}.txt; fi
+        echo "NA" >> JHU/${dti}_JHU_${padroi}.txt
+        paste JHU/*${dti}_JHU_*.txt > JHU/all_${dti}_jhu.txt
+      done
+    fi
+  done
+
+  echo "Completed: Calculating native FA, MD, AD, RD in JHU labels ${subject}"
+  echo "Diffusion pipeline completed at $(date)"
