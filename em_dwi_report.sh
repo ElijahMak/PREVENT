05c7add838a1ee5d19bcfb411bb4edebf4eede97
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-11-09 12:08:52 +0000

    a

--- em_dwi_report.sh
+++ em_dwi_report.sh
@@ -1,6 +1,6 @@
 i=${1}
 
-tbss_fill ${i}.nii 1.3 mean_FA.nii.gz  filled_${i}.nii
+tbss_fill ${i}.nii 1.3 mean_FA.nii.gz filled_${i}.nii
 
 deformationScalarVolume -in ${i}.nii -trans mean_combined.df.nii.gz -target IITmean_tensor_256.nii.gz -out ${i}_IIT_NN.nii -vsize 1 1 1 -interp 1
 fslorient -setsform 1 0 0 -128 0 1 0 -145 0 0 1 -109 0 0 0 1 ${i}_IIT_NN.nii
@@ -8,11 +8,11 @@ fslorient -setsform 1 0 0 -128 0 1 0 -145 0 0 1 -109 0 0 0 1 ${i}_IIT_NN.nii
 deformationScalarVolume -in filled_${i}.nii -trans mean_combined.df.nii.gz -target IITmean_tensor_256.nii.gz -out filled_${i}_IIT_NN.nii -vsize 1 1 1 -interp 1
 fslorient -setsform 1 0 0 -128 0 1 0 -145 0 0 1 -109 0 0 0 1 filled_${i}_IIT_NN.nii
 
-autoaq -i ${i}_IIT_NN.nii -a "JHU White-Matter Tractography Atlas" -t 1.3 -o ${i}_IIT.txt -p
+autoaq -i ${i}_IIT_NN.nii.gz -a "JHU White-Matter Tractography Atlas" -t 1.3 -o ${i}_IIT.txt -p
 
 
 
 applywarp -i ${i}.nii -o ${i}_MNI152.nii -r $FSLDIR/data/standard/FMRIB58_FA_1mm.nii.gz --premat=flirt.mat --interp=nn
-applywarp -i filled_${i}.nii -o filled_${i}_MNI152.nii -r $FSLDIR/data/standard/FMRIB58_FA_1mm.nii.gz --premat=flirt.mat --interp=nn
+applywarp -i filled_${i}.nii.gz -o filled_${i}_MNI152.nii.gz -r $FSLDIR/data/standard/FMRIB58_FA_1mm.nii.gz --premat=flirt.mat --interp=nn
 
-autoaq -i ${i}_MNI152.nii -a "JHU White-Matter Tractography Atlas" -t 1.3 -o ${i}_MNI152.txt -p
+autoaq -i ${i}_MNI152.nii.gz -a "JHU White-Matter Tractography Atlas" -t 1.3 -o ${i}_MNI152.txt -p
