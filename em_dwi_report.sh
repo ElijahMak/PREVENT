5a3e3fa7db2bfd985975311013ede22b5297346f
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-11-09 12:35:19 +0000

    a

--- em_dwi_report.sh
+++ em_dwi_report.sh
@@ -1,6 +1,7 @@
 module unload fsl/5.0.10
 module load fsl/6.0.3
 
+# DTITK
 i=${1}
 
 tbss_fill ${i}.nii 1.3 mean_FA.nii.gz filled_${i}.nii
@@ -12,12 +13,13 @@ deformationScalarVolume -in filled_${i}.nii -trans mean_combined.df.nii.gz -targ
 fslorient -setsform 1 0 0 -128 0 1 0 -145 0 0 1 -109 0 0 0 1 filled_${i}_IIT_NN.nii
 
 autoaq -i ${i}_IIT_NN.nii -a "JHU White-Matter Tractography Atlas" -t 1.3 -o ${i}_IIT_peak.txt -p
-
+autoaq -i ${i}_IIT_NN.nii -a "JHU White-Matter Tractography Atlas" -t 1.3 -o ${i}_IIT_mass.txt
 
 # FSL
-# fkm24@wbic-gate-1:~/scratch/projects/prevent_dtitk$ flirt -in mean_final_high_res.nii.gz -ref $FSLDIR/data/standard/FMRIB58_FA_1mm.nii.gz -omat flirt.mat -bins 257 -cost corratio -dof 12 -searchrx -90 90 -searchry -90 90 -searchrz -90 90
+# fkm24@wbic-gate-1:~/scratch/projects/prevent_dtitk$ flirt -in mean_FA.nii.gz -ref $FSLDIR/data/standard/FMRIB58_FA_1mm.nii.gz -omat flirt.mat -bins 257 -cost corratio -dof 12 -searchrx -90 90 -searchry -90 90 -searchrz -90 90
 
 applywarp -i ${i}.nii -o ${i}_MNI152.nii.gz -r $FSLDIR/data/standard/FMRIB58_FA_1mm.nii.gz --premat=flirt.mat --interp=nn
 applywarp -i filled_${i}.nii -o filled_${i}_MNI152.nii.gz -r $FSLDIR/data/standard/FMRIB58_FA_1mm.nii.gz --premat=flirt.mat --interp=nn
 
 autoaq -i ${i}_MNI152.nii.gz -a "JHU White-Matter Tractography Atlas" -t 1.3 -o ${i}_MNI152_peak.txt -p
+autoaq -i ${i}_MNI152.nii.gz -a "JHU White-Matter Tractography Atlas" -t 1.3 -o ${i}_MNI152_mass.txt
