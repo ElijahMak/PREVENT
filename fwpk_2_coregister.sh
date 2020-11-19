eda1fc17f123ff5c5e0b3e3f000f07dd963783e1
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-11-19 17:31:44 +0000

    a

--- fwpk_2_coregister.sh
+++ fwpk_2_coregister.sh
@@ -1,10 +1,14 @@
 # Register b0 to T1
 # Warp FW to T1
 
+cd /archive/p00423/PREVENT_Elijah/FWPK
+
 i=${1}
+
 cd ${i}
-# Register FA to T1
-flirt -in ${i}_b0.nii.gz -ref ${i}_cat12_brain.nii -dof 6 -cost normmi -omat ${i}_b0_flirt_cat12_brain.mat -out ${i}_b0_flirt.nii.gz
+
+# Register b0 to T1
+flirt -in denoised_degibbs_dwi_b0.nii -ref ${i}_cat12_brain.nii.gz -dof 6 -cost normmi -omat b0_flirt_cat12_brain.mat -out denoised_degibbs_dwi_b0_flirt.nii
 
 # Warp FW maps
-applywarp -i ${i}.FW.nii.gz -o ${i}.FW.FLIRT.nii.gz  --premat=${i}_b0_flirt_cat12_brain.mat -r ${i}_cat12_brain.nii
+applywarp -i noddi_fiso.nii.gz -o noddi_fiso_t1.nii.gz  --premat=b0_flirt_cat12_brain.mat -r ${i}_cat12_brain.nii.gz
