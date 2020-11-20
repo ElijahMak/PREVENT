aa0d1f572337f9115e3cc47c8739dbf97fb6b009
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-11-20 23:02:19 +0000

    a

--- em_petsurfer.sh
+++ em_petsurfer.sh
@@ -21,7 +21,7 @@ mov="/lustre/archive/p00423/PREVENT_Elijah/NeuroimageClinical_TauWM/pet/${subjec
 pet_mcf="/lustre/archive/p00423/PREVENT_Elijah/NeuroimageClinical_TauWM/pet/${subject}_concat_tau_mcf.nii"
 reg="$SUBJECTS_DIR/${subject}/tau/mri_coreg_concat_tau_mcf_mean_reg.lta"
 gtmseg="$SUBJECTS_DIR/${subject}/mri/gtmseg.mgz"
-mgxthresh="0.01"
+mgxthresh="0.01s"
 tac="$SUBJECTS_DIR/dynamic_pet/PET_$tac/tac*.dat"
 hemi="/lustre/archive/p00423/PREVENT_Elijah/NeuroimageClinical_TauWM/freesurfer/hemi"
 fwhm="8"
@@ -51,15 +51,15 @@ echo "Coregister mean volumes to T1"
 # for i in `cat frame_54`; do cp tac_54.dat ../freesurfer/include/${subject}/tau/tac.dat; done
 # for i in `cat frame_52`; do cp tac_52.dat ../freesurfer/include/${subject}/tau/tac.dat; done
 
-#mri_gtmpvc --i ${pet_mcf}  --reg ${reg} --psf 6.8 --seg $SUBJECTS_DIR/${subject}/mri/gtmseg.mgz --default-seg-merge --auto-mask PSF .01 --mgx $mgxthresh --o $SUBJECTS_DIR/${subject}/tau/dynamic_gtm_thresh_${mgxthresh}/ --km-ref 8 47 --km-hb 11 12 13 50 51 52 --no-rescale
+mri_gtmpvc --i ${pet_mcf}  --reg ${reg} --psf 6.8 --seg $SUBJECTS_DIR/${subject}/mri/gtmseg.mgz --default-seg-merge --auto-mask PSF .01 --mgx $mgxthresh --o $SUBJECTS_DIR/${subject}/tau/dynamic_gtm_thresh_${mgxthresh}/ --km-ref 8 47 --km-hb 11 12 13 50 51 52 --no-rescale
 
-# mri_glmfit --y $SUBJECTS_DIR/${subject}/tau/dynamic_gtm_thresh_${mgxthresh}/km.hb.tac.nii.gz --mrtm1 $SUBJECTS_DIR/${subject}/tau/dynamic_gtm_thresh_${mgxthresh}/km.ref.tac.dat $SUBJECTS_DIR/${subject}/tau/tac.dat --o $SUBJECTS_DIR/${subject}/tau/dynamic_gtm_thresh_${mgxthresh}/mrtm1 --no-est-fwhm  --nii.gz
+ mri_glmfit --y $SUBJECTS_DIR/${subject}/tau/dynamic_gtm_thresh_${mgxthresh}/km.hb.tac.nii.gz --mrtm1 $SUBJECTS_DIR/${subject}/tau/dynamic_gtm_thresh_${mgxthresh}/km.ref.tac.dat $SUBJECTS_DIR/${subject}/tau/tac.dat --o $SUBJECTS_DIR/${subject}/tau/dynamic_gtm_thresh_${mgxthresh}/mrtm1 --no-est-fwhm  --nii.gz
 
-#k2p=`cat $SUBJECTS_DIR/${subject}/tau/dynamic_gtm_thresh_${mgxthresh}/mrtm1/k2prime.dat`
+k2p=`cat $SUBJECTS_DIR/${subject}/tau/dynamic_gtm_thresh_${mgxthresh}/mrtm1/k2prime.dat`
 
-# mri_glmfit --y $SUBJECTS_DIR/${subject}/tau/dynamic_gtm_thresh_${mgxthresh}/gtm.nii.gz --mrtm2 $SUBJECTS_DIR/${subject}/tau/dynamic_gtm_thresh_${mgxthresh}/km.ref.tac.dat  $SUBJECTS_DIR/${subject}/tau/tac.dat $k2p --o $SUBJECTS_DIR/${subject}/tau/dynamic_gtm_thresh_${mgxthresh}/mrtm2 --no-est-fwhm --nii.gz
+mri_glmfit --y $SUBJECTS_DIR/${subject}/tau/dynamic_gtm_thresh_${mgxthresh}/gtm.nii.gz --mrtm2 $SUBJECTS_DIR/${subject}/tau/dynamic_gtm_thresh_${mgxthresh}/km.ref.tac.dat  $SUBJECTS_DIR/${subject}/tau/tac.dat $k2p --o $SUBJECTS_DIR/${subject}/tau/dynamic_gtm_thresh_${mgxthresh}/mrtm2 --no-est-fwhm --nii.gz
 
-# for h in `cat ${hemi} `; do mri_vol2surf --mov ${SUBJECTS_DIR}/${subject}/tau/dynamic_gtm_thresh_${mgxthresh}/mgx.ctxgm.nii.gz --reg ${SUBJECTS_DIR}/${subject}/tau/dynamic_gtm_thresh_${mgxthresh}/aux/bbpet2anat.lta --hemi ${h} --projfrac 0.5 --o $SUBJECTS_DIR/${subject}/surf/${h}.tau.mgx.ctxgm.fsaverage.mgx.thresh.${mgxthresh}.sm00.nii.gz --cortex --trgsubject fsaverage; mris_fwhm --smooth-only --i $SUBJECTS_DIR/${subject}/surf/${h}.tau.mgx.ctxgm.fsaverage.mgx.thresh.${mgxthresh}.sm00.nii.gz  --fwhm $fwhm --o $SUBJECTS_DIR/${subject}/surf/${h}.tau.mgx.ctxgm.fsaverage.mgx.thresh.${mgxthresh}.sm0${fwhm}.nii.gz  --cortex --s fsaverage --hemi ${h}; done
+for h in `cat ${hemi} `; do mri_vol2surf --mov ${SUBJECTS_DIR}/${subject}/tau/dynamic_gtm_thresh_${mgxthresh}/mgx.ctxgm.nii.gz --reg ${SUBJECTS_DIR}/${subject}/tau/dynamic_gtm_thresh_${mgxthresh}/aux/bbpet2anat.lta --hemi ${h} --projfrac 0.5 --o $SUBJECTS_DIR/${subject}/surf/${h}.tau.mgx.ctxgm.fsaverage.mgx.thresh.${mgxthresh}.sm00.nii.gz --cortex --trgsubject fsaverage; mris_fwhm --smooth-only --i $SUBJECTS_DIR/${subject}/surf/${h}.tau.mgx.ctxgm.fsaverage.mgx.thresh.${mgxthresh}.sm00.nii.gz  --fwhm $fwhm --o $SUBJECTS_DIR/${subject}/surf/${h}.tau.mgx.ctxgm.fsaverage.mgx.thresh.${mgxthresh}.sm0${fwhm}.nii.gz  --cortex --s fsaverage --hemi ${h}; done
 
 for h in `cat hemi `;  do  k2p=`cat $SUBJECTS_DIR/${subject}/tau/dynamic_gtm_thresh_${mgxthresh}/mrtm1/k2prime.dat`; mri_glmfit --y $SUBJECTS_DIR/${subject}/surf/${h}.tau.mgx.ctxgm.fsaverage.mgx.thresh.${mgxthresh}.sm0${fwhm}.nii.gz --mrtm2 $SUBJECTS_DIR/${subject}/tau/dynamic_gtm_thresh_${mgxthresh}/km.ref.tac.dat $SUBJECTS_DIR/${subject}/tau/tac.dat $k2p --o $SUBJECTS_DIR/${subject}/tau/dynamic_gtm_thresh_${mgxthresh}/mrtm2/${h}.sm0${fwhm} --no-est-fwhm --nii.gz --surface fsaverage ${h}; dsone
 
