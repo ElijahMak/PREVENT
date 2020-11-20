f1450221423e7a5d34d0a35c4d258ed2edd839c5
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-11-20 11:09:09 +0000

    a

--- em_petsurfer_bach.sh
+++ em_petsurfer_bach.sh
@@ -12,10 +12,11 @@ for subject in `cat $input`;
 
 do
 
-sbatch --exclusive --job-name=noddi --account MAK-SL3-GPU --partition pascal --error=${subject%.*}_%j.err \
---output=${subject%.*}_%j.out --time=${time} --nodes=1 --ntasks-per-node=$nTask --mem=2500 \
---wrap="bash /home/fkm24/em_code/em_petsurfer.sh $subject "
+  sbatch --job-name=fs7_t1t2bfc --account hphi --qos=long.q --partition wbic-cs --error=${subject%.*}_%j.err \
+  --output=${subject%.*}_%j.out --time=${time} --nodes=1 --ntasks-per-node=${nTask} --mem=${mem} \
+  --wrap="bash ${code}/em_petsurfer.sh ${subject}"
 
-sleep 1
+  done
+=
 
 done
