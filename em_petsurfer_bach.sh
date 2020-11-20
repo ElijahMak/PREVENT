ae8d77093218a039d6864de0156dc295dd7d1fec
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-11-20 11:07:52 +0000

    a

--- em_petsurfer_bach.sh
+++ em_petsurfer_bach.sh
@@ -14,7 +14,7 @@ do
 
 sbatch --exclusive --job-name=noddi --account MAK-SL3-GPU --partition pascal --error=${subject%.*}_%j.err \
 --output=${subject%.*}_%j.out --time=${time} --nodes=1 --ntasks-per-node=$nTask --mem=2500 \
---wrap="bash /home/fkm24/em_code/em_petsurfer.sh $subject 1"
+--wrap="bash /home/fkm24/em_code/em_petsurfer.sh $subject "
 
 sleep 1
 
