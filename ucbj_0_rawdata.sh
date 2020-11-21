3ef387b6af2483192e03115616f480e55568addc
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-11-21 12:22:33 +0000

    a

--- ucbj_0_rawdata.sh
+++ ucbj_0_rawdata.sh
@@ -1,4 +1,7 @@
-# Thi
+# This script was created to process the new batch of subjecst as part of the UCBJ x NODDI NBA major revisions.
+# Author : Elijah Make
+
+# NIIs were obtained from NEgin Holland.
 
 subject=${1}
 
@@ -9,11 +12,10 @@ cd $noddi_dir
 
 # Copy T1 MPRAGE ND
 cp unprocessed_mri_for_noddi_ucbj_mans_131120/sub-${subject}/ses-*/anat/sub-${subject}_*_acq-mpragesag_rec-nd* ${subject}
-cp unprocessed_mri_for_noddi_ucbj_mans_131120/sub-${subject}/ses-*/anat/sub-${subject}_*_acq-mpragesag_rec-nd.nii.gz ${subject}/${subject}.nii.gz
+cp unprocessed_mri_for_noddi_ucbj_mans_131120/sub-${subject}/ses-*/anat/sub-${subject}_*_acq-mpragesag_rec-nd*.nii.gz ${subject}/${subject}.nii.gz
 
 
 # Copy NODDI
-
 # Copy AP
 cp unprocessed_mri_for_noddi_ucbj_mans_131120/sub-${subject}/ses-*/dwi/sub-${subject}_*_acq-noddiapcbumbep2d175mmmb498dirb300b1000b2000_run-01_dwi.nii.gz ${subject}/ap.nii
 cp unprocessed_mri_for_noddi_ucbj_mans_131120/sub-${subject}/ses-*/dwi/sub-${subject}_*_acq-noddiapcbumbep2d175mmmb498dirb300b1000b2000_run-01_dwi.bval ${subject}/ap.bval
