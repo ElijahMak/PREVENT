47d1d0e2eb64bf2740217f0c57efce1eeda8b018
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-11-21 18:58:57 +0000

    a

--- ucbj_1_dwi_1.sh
+++ ucbj_1_dwi_1.sh
@@ -22,8 +22,8 @@ subject=${1}
 raw_dwi="all_DWIs.mif"
 denoised_dwi="all.dwi.denoised.mif"
 denoised_degibbs_dwi="dwi.denoised.degibbs.mif"
-denoised_degibbs_preproc_dwi="dwi.denoised.debibbs.preproc.mif"
-denoised_degibbs_preproc_bfc_dwi="dwi.denoised.debibbs.preproc.bfc.mif"
+denoised_degibbs_preproc_dwi="dwi.denoised.degibbs.preproc.mif"
+denoised_degibbs_preproc_bfc_dwi="dwi.denoised.degibbs.preproc.bfc.mif"
 mask="mask.nii"
 b0="denoised_degibbs_preproc_bfc_dwi_b0.nii"
 edc_mif="denoised_degibbs.edc.repol.mif"
@@ -93,7 +93,7 @@ echo "--------------------------------"
 # Remove Gibbs Ringing artifact
 mrdegibbs ${denoised_dwi} ${denoised_degibbs_dwi} -bias
 
-mrcalc dwi_den.mif dwi_den_unr.mif –subtract residualUnringed.mif
+#mrcalc dwi_den.mif dwi_den_unr.mif –subtract residualUnringed.mif
 # mrview dwi_den_unr.mif residualUnringed.mif
 
 echo "--------------------------------"
@@ -102,48 +102,47 @@ echo "--------------------------------"
 
 dwifslpreproc ${denoised_degibbs_dwi} ${denoised_degibbs_preproc_dwi} -rpe_header -eddyqc_all eddyqc -eddy_options '  --repol --data_is_shelled --slm=linear --niter=5'
 
-
 mrconvert ${denoised_degibbs_preproc_dwi} -strides -1,2,3,4 -export_grad_fsl bvecs bvals dwi.nii
 
-# Bias-field correction and tensor fitting
-# -------------------------------------------------------------------------------------------------
-
-echo "================================"
-echo "       Bias field correction    "
-echo "================================"
-
-# Perform bias-field correction
-dwibiascorrect ants -bias bias.mif ${denoised_degibbs_preproc_dwi} ${denoised_degibbs_preproc_bfc_dwi}
-
-echo "================================"
-echo "          MRTRIX Brain mask     "
-echo "================================"
-
-dwi2mask ${denoised_degibbs_preproc_bfc_dwi} ${mask}
-
-echo "--------------------------------"
-echo "            FSLBET              "
-echo "--------------------------------"
-
-fslroi dwi.nii ${b0} 0 1
-
-for f in 0.1 0.2 0.3 0.4
-    do
-     bet ${b0} denoised_degibbs_preproc_bfc_dwi_b0_brain_f${f} -m -f $f
-    done
-
-echo "--------------------------------"
-echo "      Generate screenshots      "
-echo "--------------------------------"
-
- mkdir qc_brainmasks
-
- for f in 0.1 0.2 0.3 0.4; do
-
-   outfile="qc_brainmasks/b0.mask.f${f}.png"
-   qc_mask="denoised_degibbs_preproc_bfc_dwi_b0_brain_f${f}.nii"
-
-   pkill Xvfb
-   xvfb-run -s "-screen 0, 1024x768x24" fsleyes render --scene lightbox --outfile $outfile --size 2500 2500 --worldLoc 108.2101927736983 124.64060310792524 -42.83765119809547 --displaySpace ${b0} --zaxis 2 --sliceSpacing 2.0 --zrange -1.0 124.99980163574219 --ncols 10 --nrows 10 --bgColour 0.0 0.0 0.0 --fgColour 1.0 1.0 1.0 --hideCursor --cursorColour 0.0 1.0 0.0 --colourBarLocation top --colourBarLabelSide top-left --performance 3 --movieSync ${b0} --name "b0" --overlayType volume --alpha 100.0 --brightness 67.02551834130782 --contrast 84.05103668261563 --cmap greyscale --negativeCmap greyscale --displayRange 0.0 800.0 --clippingRange 0.0 2533.08 --gamma 0.0 --cmapResolution 256 --interpolation none --numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0 ${qc_mask} --name "b0_brain_mask" --overlayType volume --alpha 47.76828976994573 --brightness 50.0 --contrast 50.0 --cmap blue-lightblue --negativeCmap greyscale --displayRange 0.0 1.0 --clippingRange 0.0 1.01 --gamma 0.0 --cmapResolution 256 --interpolation none --numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0
-
- done
+# # Bias-field correction and tensor fitting
+# # -------------------------------------------------------------------------------------------------
+#
+# echo "================================"
+# echo "       Bias field correction    "
+# echo "================================"
+#
+# # Perform bias-field correction
+# dwibiascorrect ants -bias bias.mif ${denoised_degibbs_preproc_dwi} ${denoised_degibbs_preproc_bfc_dwi}
+#
+# echo "================================"
+# echo "          MRTRIX Brain mask     "
+# echo "================================"
+#
+# dwi2mask ${denoised_degibbs_preproc_bfc_dwi} ${mask}
+#
+# echo "--------------------------------"
+# echo "            FSLBET              "
+# echo "--------------------------------"
+#
+# fslroi dwi.nii ${b0} 0 1
+#
+# for f in 0.1 0.2 0.3 0.4
+#     do
+#      bet ${b0} denoised_degibbs_preproc_bfc_dwi_b0_brain_f${f} -m -f $f
+#     done
+#
+# echo "--------------------------------"
+# echo "      Generate screenshots      "
+# echo "--------------------------------"
+#
+#  mkdir qc_brainmasks
+#
+#  for f in 0.1 0.2 0.3 0.4; do
+#
+#    outfile="qc_brainmasks/b0.mask.f${f}.png"
+#    qc_mask="denoised_degibbs_preproc_bfc_dwi_b0_brain_f${f}.nii"
+#
+#    pkill Xvfb
+#    xvfb-run -s "-screen 0, 1024x768x24" fsleyes render --scene lightbox --outfile $outfile --size 2500 2500 --worldLoc 108.2101927736983 124.64060310792524 -42.83765119809547 --displaySpace ${b0} --zaxis 2 --sliceSpacing 2.0 --zrange -1.0 124.99980163574219 --ncols 10 --nrows 10 --bgColour 0.0 0.0 0.0 --fgColour 1.0 1.0 1.0 --hideCursor --cursorColour 0.0 1.0 0.0 --colourBarLocation top --colourBarLabelSide top-left --performance 3 --movieSync ${b0} --name "b0" --overlayType volume --alpha 100.0 --brightness 67.02551834130782 --contrast 84.05103668261563 --cmap greyscale --negativeCmap greyscale --displayRange 0.0 800.0 --clippingRange 0.0 2533.08 --gamma 0.0 --cmapResolution 256 --interpolation none --numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0 ${qc_mask} --name "b0_brain_mask" --overlayType volume --alpha 47.76828976994573 --brightness 50.0 --contrast 50.0 --cmap blue-lightblue --negativeCmap greyscale --displayRange 0.0 1.0 --clippingRange 0.0 1.01 --gamma 0.0 --cmapResolution 256 --interpolation none --numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0
+#
+#  done
