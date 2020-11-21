30b40b6b79c994fd1a1af22cf7ca723cb728083b
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-11-21 16:39:14 +0000

    a

--- ucbj_1_dwi_1.sh
+++ ucbj_1_dwi_1.sh
@@ -1,5 +1,6 @@
 # MRTRIX pipeline
-# Perform denoising, degibbs and BET masks
+# Perform denoising, degibbs, top up and eddy corrections from DICOMs. First convert everything to .mif, and then concatenate the results, in which case MRtrix3 handles concatenation of the gradient tables automatically.
+
 
 # Modules
 # -----------------------------------------------------------------------------
@@ -16,18 +17,14 @@ module load MRtrix/mrtrix-3.0.2
 # Options
 # dir="/lustre/archive/p00423/PREVENT_Elijah/MRTRIX"
 subject=${1}
-BET=${2}
 
 # Inputs
-raw_dwi="dwi.nii"
-raw_dwi_mif="dwi.mif"
-denoised_dwi="dwi.denoised.nii"
-denoised_degibbs_dwi="dwi.denoised.degibbs.nii"
-b0="denoised_degibbs_dwi_b0.nii"
-mask="denoised_degibbs_dwi_b0_brain_f0.2_mask.nii"
-output_edc="denoised_degibbs.edc.repol"
-edc="denoised_degibbs.edc.repol.nii"
-edc_rotated_bvecs="denoised_degibbs.edc.repol.eddy_rotated_bvecs"
+raw_dwi="all_DWIs.mif"
+denoised_dwi="all.dwi.denoised.mif"
+denoised_degibbs_dwi="dwi.denoised.degibbs.mif"
+denoised_degibbs_preproc_dwi="dwi.denoised.debibbs.preproc.mif"
+denoised_degibbs_preproc_bfc_dwi="dwi.denoised.debibbs.preproc.bfc.mif"
+mask="mask.nii"
 edc_mif="denoised_degibbs.edc.repol.mif"
 edc_mif_bfc="denoised_degibbs.edc.repol.bfc.mif"
 edc_mif_bfc_tensor="denoised_degibbs.edc.repol.bfc.tensor.mif"
@@ -57,17 +54,29 @@ echo "Initialising diffusion preprocessing for ${subject}"
 # Preprocessing
 # -------------------------------------------------------------------------------------------------
 echo "================================"
-echo "         Preprocessing          "
+echo "            NII to MIF          "
 echo "================================"
 
-# cd ${dir}
 cd ${subject}
 
-echo "--------------------------------"
-echo "          NII to MIF            "
-echo "--------------------------------"
+cd *AP*
+for img in **
+do
+dcmdjpeg $img ${img}.tmp
+mv ${img}.tmp $img
+done
 
-mrconvert ${raw_dwi} ${raw_dwi_mif} -fslgrad bvec bval -datatype float32 -strides 0,0,0,1
+cd ../*PA*
+for img in **
+do
+dcmdjpeg $img ${img}.tmp
+mv ${img}.tmp $img
+done
+
+cd ../..
+
+# Concatenate both DWI dicom folders
+mrcat *AP* *PA* ${raw_dwi} -axis 3
 
 echo "--------------------------------"
 echo "          Denoising             "
@@ -81,36 +90,29 @@ echo "            DeGibbs             "
 echo "--------------------------------"
 
 # Remove Gibbs Ringing artifact
-mrdegibbs ${denoised_dwi} ${denoised_degibbs_dwi}
+mrdegibbs ${denoised_dwi} ${denoised_degibbs_dwi} -bias
+
+mrcalc dwi_den.mif dwi_den_unr.mif –subtract residualUnringed.mif
+# mrview dwi_den_unr.mif residualUnringed.mif
 
 echo "--------------------------------"
-echo "              BET               "
+echo "           dwifslpreproc        "
 echo "--------------------------------"
 
-# Perform brain extraction
-if [ "${BET}" = "0" ]; then
-   echo "Running BET [1/3]"
-   echo "fslroi $denoised_degibbs_dwi $b0 0 1"
-   fslroi ${denoised_degibbs_dwi} ${b0} 0 1
+dwifslpreproc ${denoised_degibbs_dwi} ${denoised_degibbs_preproc_dwi} -rpe_header -eddyqc_all eddyqc -eddy_options '  --repol --data_is_shelled --slm=linear --niter=5'
 
-   for f in 0.1 0.2 0.3 0.4; do
-     bet ${b0} denoised_degibbs_dwi_b0_brain_f${f} -m -f $f; done
- else
-   echo "Using edited BET [1/3]"
- fi
+# Bias-field correction and tensor fitting
+# -------------------------------------------------------------------------------------------------
 
- echo "--------------------------------"
- echo "      Generate screenshots      "
- echo "--------------------------------"
+echo "================================"
+echo "       BIAS FIELD correction    "
+echo "================================"
 
- mkdir QC
+# Perform bias-field correction
+dwibiascorrect ants -bias bias.mif ${denoised_degibbs_preproc_dwi} ${denoised_degibbs_preproc_bfc_dwi}
 
- for f in 0.1 0.2 0.3 0.4; do
+echo "================================"
+echo "          Brain mask            "
+echo "================================"
 
-   outfile="QC/b0.mask.f${f}.png"
-   qc_mask="denoised_degibbs_dwi_b0_brain_f${f}_mask.nii"
-
-   pkill Xvfb
-   xvfb-run -s "-screen 0, 1024x768x24" fsleyes render --scene lightbox --outfile $outfile --size 2500 2500 --worldLoc 108.2101927736983 124.64060310792524 -42.83765119809547 --displaySpace ${b0} --zaxis 2 --sliceSpacing 2.0 --zrange -1.0 124.99980163574219 --ncols 10 --nrows 10 --bgColour 0.0 0.0 0.0 --fgColour 1.0 1.0 1.0 --hideCursor --cursorColour 0.0 1.0 0.0 --colourBarLocation top --colourBarLabelSide top-left --performance 3 --movieSync ${b0} --name "b0" --overlayType volume --alpha 100.0 --brightness 67.02551834130782 --contrast 84.05103668261563 --cmap greyscale --negativeCmap greyscale --displayRange 0.0 800.0 --clippingRange 0.0 2533.08 --gamma 0.0 --cmapResolution 256 --interpolation none --numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0 ${qc_mask} --name "b0_brain_mask" --overlayType volume --alpha 47.76828976994573 --brightness 50.0 --contrast 50.0 --cmap blue-lightblue --negativeCmap greyscale --displayRange 0.0 1.0 --clippingRange 0.0 1.01 --gamma 0.0 --cmapResolution 256 --interpolation none --numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0
-
- done
+dwi2mask ${denoised_degibbs_preproc_bfc_dwi} ${mask}
