5ad7925f377f84f527cb0bca666e6323883e3918
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-11-23 15:31:51 +0000

    a

--- ucbj_surface_mapping.sh
+++ ucbj_surface_mapping.sh
@@ -1,49 +1,54 @@
 # DWI preprocessing, using a single B0 from PA to correct distortions in AP.
 # Author: Elijah Make
 
-# Directory
+# # Directory
+# # --------------------------------------------
+# dir="/scratch/hphi/fkm24/projects/ucbjxnoddi/noddi/"
+#
+# # Modules
+# # --------------------------------------------
+# module load freesurfer/7.1.0
+# module load ANTS/2.2.0
+# module unload fsl/5.0.10
+# module load fsl/6.0.3
+# module load MRtrix/mrtrix-3.0.2
+#
+# # Parameters
+# # --------------------------------------------
+# subject=${1}
+# SUBJECTS_DIR="/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/freesurfer"
+#
+# # Coregister UCBJ to T1
+# # --------------------------------------------
+# mri_coreg --s ${subject} --mov /archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/${subject}/${subject}_UCB-J_PVC_R1_Radio.nii --reg $SUBJECTS_DIR/${subject}/ucbj2/coreg_pvc_r1.lta
+#
+# # Project pvc volume to surface
+# # --------------------------------------------
+#
+# for h in lh rh; do
+#
+# mov="/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/${subject}/${subject}_UCB-J_PVC_BP_Radio.nii"
+# reg="${SUBJECTS_DIR}/${subject}/ucbj2/coreg_pvc_r1.lta"
+# projfrac="0.5"
+# output="${SUBJECTS_DIR}/${subject}/ucbj2/${h}_pvc_ucbj_bp.nii.gz"
+# mri_vol2surf --mov $mov --reg $reg --hemi ${h} --projfrac 0.5 --o $output --cortex --trgsubject fsaverage; done
+#
+# for h in lh rh; do
+# fwhm="8"
+# input="${SUBJECTS_DIR}/${subject}/ucbj2/${h}_pvc_ucbj_bp.nii.gz"
+# output="${SUBJECTS_DIR}/${subject}/ucbj2/${h}_pvc_ucbj_bp_sm${fwhm}.nii.gz"
+# mris_fwhm --smooth-only --i $input --fwhm $fwhm --o $output  --cortex --s fsaverage --hemi ${h}; done
+
+# Coregister UNPVC UCBJ to T1
 # --------------------------------------------
-dir="/scratch/hphi/fkm24/projects/ucbjxnoddi/noddi/"
-
-# Modules
-# --------------------------------------------
-module load freesurfer/7.1.0
-module load ANTS/2.2.0
-module unload fsl/5.0.10
-module load fsl/6.0.3
-module load MRtrix/mrtrix-3.0.2
-
-# Parameters
-# --------------------------------------------
-subject=${1}
-SUBJECTS_DIR="/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/freesurfer"
-# Coregister UCBJ to T1
-# --------------------------------------------
-mri_coreg --s ${subject} --mov /archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/${subject}/${subject}_UCB-J_PVC_R1_Radio.nii --reg $SUBJECTS_DIR/${subject}/ucbj2/coreg_pvc_r1.lta
-
-# Project pvc volume to surface
-# --------------------------------------------
-
-for h in lh rh; do
-
-mov="/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/${subject}/${subject}_UCB-J_PVC_BP_Radio.nii"
-reg="${SUBJECTS_DIR}/${subject}/ucbj2/coreg_pvc_r1.lta"
-projfrac="0.5"
-output="${SUBJECTS_DIR}/${subject}/ucbj2/${h}_pvc_ucbj_bp.nii.gz"
-mri_vol2surf --mov $mov --reg $reg --hemi ${h} --projfrac 0.5 --o $output --cortex --trgsubject fsaverage; done
-
-for h in lh rh; do
-fwhm="8"
-input="${SUBJECTS_DIR}/${subject}/ucbj2/${h}_pvc_ucbj_bp.nii.gz"
-output="${SUBJECTS_DIR}/${subject}/ucbj2/${h}_pvc_ucbj_bp_sm${fwhm}.nii.gz"
-mris_fwhm --smooth-only --i $input --fwhm $fwhm --o $output  --cortex --s fsaverage --hemi ${h}; done
-
-
+mri_coreg --s ${subject} --mov /archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/${subject}/${subject}_UCB-J_R1_Radio.nii --reg $SUBJECTS_DIR/${subject}/ucbj2/coreg_unpvc_r1.lta
 
 # Project un pvc to surface
 # --------------------------------------------
-mov="/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/${subject}/${subject}_UCB-J_BP_Radio.nii"
-reg="${SUBJECTS_DIR}/${subject}/ucbj2/coreg_pvc_r1.lta"
+for h in lh rh; do
+
+mov="/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/${subject}/${subject}_UCB-J_PVC_BP_Radio.nii"
+reg="${SUBJECTS_DIR}/${subject}/ucbj2/coreg_unpvc_r1.lta"
 projfrac="0.5"
 output="${SUBJECTS_DIR}/${subject}/ucbj2/${h}_unpvc_ucbj_bp.nii.gz"
 mri_vol2surf --mov $mov --reg $reg --hemi ${h} --projfrac 0.5 --o $output --cortex --trgsubject fsaverage; done
@@ -53,3 +58,39 @@ fwhm="8"
 input="${SUBJECTS_DIR}/${subject}/ucbj2/${h}_unpvc_ucbj_bp.nii.gz"
 output="${SUBJECTS_DIR}/${subject}/ucbj2/${h}_unpvc_ucbj_bp_sm${fwhm}.nii.gz"
 mris_fwhm --smooth-only --i $input --fwhm $fwhm --o $output  --cortex --s fsaverage --hemi ${h}; done
+
+# Project PVC volume
+# --------------------------------------------
+
+mov="/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/${subject}/${subject}_UCB-J_PVC_BP_Radio.nii"
+reg="${SUBJECTS_DIR}/${subject}/ucbj2/coreg_pvc_r1.lta"
+targ=$SUBJECTS_DIR/${subject}/mri/brainmask.mgz
+output="${SUBJECTS_DIR}/${subject}/ucbj2/vol_pvc_ucbj_bp.nii.gz"
+mni152_output="${SUBJECTS_DIR}/${subject}/ucbj2/mni_vol_pvc_ucbj_bp.nii.gz"
+sm_output="${SUBJECTS_DIR}/${subject}/ucbj2/vol_pvc_ucbj_bp_sm${fwhm}.nii.gz"
+sm_mni152_output="${SUBJECTS_DIR}/${subject}/ucbj2/mni_vol_pvc_ucbj_bp_sm${fwhm}.nii.gz"
+fwhm="8"
+
+mri_vol2vol --mov $mov --reg $reg --targ ${targ} --o $output
+mri_vol2vol --mov $mov --reg $reg --targ ${targ} --o ${mni152_output} --mni152reg
+
+mri_fwhm --smooth-only --i $output --fwhm $fwhm --o $sm_output
+mri_fwhm --smooth-only --i $mni152_output --fwhm $fwhm --o $sm_mni152_output
+
+# Project UNPVC volume
+# --------------------------------------------
+
+mov="/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/${subject}/${subject}_UCB-J_BP_Radio.nii"
+reg="${SUBJECTS_DIR}/${subject}/ucbj2/coreg_unpvc_r1.lta"
+targ=$SUBJECTS_DIR/${subject}/mri/brainmask.mgz
+output="${SUBJECTS_DIR}/${subject}/ucbj2/vol_unpvc_ucbj_bp.nii.gz"
+mni152_output="${SUBJECTS_DIR}/${subject}/ucbj2/mni_vol_unpvc_ucbj_bp.nii.gz"
+sm_output="${SUBJECTS_DIR}/${subject}/ucbj2/vol_unpvc_ucbj_bp_sm${fwhm}.nii.gz"
+sm_mni152_output="${SUBJECTS_DIR}/${subject}/ucbj2/mni_vol_unpvc_ucbj_bp_sm${fwhm}.nii.gz"
+fwhm="8"
+
+mri_vol2vol --mov $mov --reg $reg --targ ${targ} --o $output
+mri_vol2vol --mov $mov --reg $reg --targ ${targ} --o ${mni152_output} --mni152reg
+
+mri_fwhm --smooth-only --i $output --fwhm $fwhm --o $sm_output
+mri_fwhm --smooth-only --i $mni152_output --fwhm $fwhm --o $sm_mni152_output
