b65a88cb485594d23fe55209438d0becc46217d6
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-11-23 15:33:27 +0000

    a

--- ucbj_surface_mapping.sh
+++ ucbj_surface_mapping.sh
@@ -1,23 +1,23 @@
 # DWI preprocessing, using a single B0 from PA to correct distortions in AP.
 # Author: Elijah Make
 
-# # Directory
-# # --------------------------------------------
-# dir="/scratch/hphi/fkm24/projects/ucbjxnoddi/noddi/"
-#
-# # Modules
-# # --------------------------------------------
-# module load freesurfer/7.1.0
-# module load ANTS/2.2.0
-# module unload fsl/5.0.10
-# module load fsl/6.0.3
-# module load MRtrix/mrtrix-3.0.2
-#
-# # Parameters
-# # --------------------------------------------
-# subject=${1}
-# SUBJECTS_DIR="/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/freesurfer"
-#
+# Directory
+# --------------------------------------------
+dir="/scratch/hphi/fkm24/projects/ucbjxnoddi/noddi/"
+
+# Modules
+# --------------------------------------------
+module load freesurfer/7.1.0
+module load ANTS/2.2.0
+module unload fsl/5.0.10
+module load fsl/6.0.3
+module load MRtrix/mrtrix-3.0.2
+
+# Parameters
+# --------------------------------------------
+subject=${1}
+SUBJECTS_DIR="/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/freesurfer"
+
 # # Coregister UCBJ to T1
 # # --------------------------------------------
 # mri_coreg --s ${subject} --mov /archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/${subject}/${subject}_UCB-J_PVC_R1_Radio.nii --reg $SUBJECTS_DIR/${subject}/ucbj2/coreg_pvc_r1.lta
