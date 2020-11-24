8e8a4afcd255d7c235c7824e1bce254ef378e7e8
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-11-24 14:21:16 +0000

    a

---
+++ em_mricoreg.sh
@@ -0,0 +1,21 @@
+# Register data to T1
+#!/bin/bash
+
+# Load modules
+module unload fsl/5.0.10
+module load fsl/6.0.1
+module load freesurfer/7.1.0
+subject=${1}
+mov="${subject}_AV-1451_R1_Neuro"
+brain="${subject}.brain.nii.gz"
+fwhm="4"
+
+cd $subject
+
+mri_coreg --mov ${mov}.nii --ref ${brain} --reg ${mov}_coreg.lta
+mri_vol2vol --mov ${mov}.nii --targ ${brain} --lta ${mov}_coreg.lta --trilin --o ${mov}_Anat.nii
+fslmaths ${mov}_Anat.nii -kernel gauss ${fwhm} -fmean ${mov}_Anat_sm${fwmh}.nii
+#create a binary mask for the non-zero voxels
+fslmaths ${subject}.brain.mask.nii.gz -kernel gauss ${fwhm} -fmean ${subject}.brain.mask.sm${fwmh}.nii
+#divide the result from step 1 by the result from step 2 and save this
+fslmaths ${mov}_Anat_sm${fwmh}.nii -div ${subject}.brain.mask.sm${fwmh}.nii  ${mov}_Anat_sm${fwmh}.out.nii
