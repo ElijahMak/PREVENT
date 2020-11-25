60463250841e86bf5c4c99497e7dc9f2f8243853
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-11-25 11:16:58 +0000

    a

--- asl_mri_segstats.sh
+++ asl_mri_segstats.sh
@@ -10,6 +10,7 @@ module load freesurfer/7.1.0
 export SUBJECTS_DIR=/lustre/archive/p00423/PREVENT_Elijah/Neuroimage_ASLxR1/freesurfer
 subject=${1}
 r1="/lustre/archive/p00423/PREVENT_Elijah/Neuroimage_ASLxR1/${subject}/${subject}_AV-1451_R1_Radio"
+av="/lustre/archive/p00423/PREVENT_Elijah/Neuroimage_ASLxR1/${subject}/${subject}_AV-1451_BP_Radio"
 targ="$SUBJECTS_DIR/${subject}/mri/aparc+aseg.mgz"
 lta="$SUBJECTS_DIR/${subject}/r1/coreg.lta"
 interp="nearest"
@@ -24,3 +25,6 @@ mri_vol2vol --mov ${r1}.nii --targ ${targ} --lta ${lta} --${interp} --o ${aparca
 
 # Extract statistics
 mri_segstats --seg $SUBJECTS_DIR/${subject}/r1/aparcaseg_r1.mgz --i ${r1}.nii --ctab $FREESURFER_HOME/FreeSurferColorLUT.txt --sum $SUBJECTS_DIR/${subject}/stats/r1_aparcaseg.stats
+
+# Extract statistics
+mri_segstats --seg $SUBJECTS_DIR/${subject}/r1/aparcaseg_r1.mgz --i ${r1}.nii --ctab $FREESURFER_HOME/FreeSurferColorLUT.txt --sum $SUBJECTS_DIR/${subject}/stats/av_aparcaseg.stats
