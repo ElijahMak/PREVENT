4ab43845a08040eef10941a16287d8bf695377a5
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-11-25 11:57:13 +0000

    a

--- em_asl.sh
+++ em_asl.sh
@@ -3,8 +3,10 @@
 #!/bin/bash
 
 # Load modules
+. /etc/profile.d/modules.sh
+module purge
 module unload fsl/5.0.10
-module load fsl/6.0.3 # Necessary, otherwise oxford_asl does not recognise iaf.
+module load fsl/6.0.3
 module load freesurfer/7.1.0
 
 # Parameters
@@ -24,31 +26,20 @@ fwhm="8"
 
 # CD subject directory
 cd ${subject}
-#
-# # Generate M0 and LC
-# fslroi ${subject}.asl.nii.gz ${subject}.asl.m0.nii.gz 0 1
-# fslroi ${subject}.asl.nii.gz ${subject}.asl.lc.nii.gz 1 90
-#
-# # Register M0 to T1 skull stripped brain
-# mri_coreg --mov ${subject}.asl.m0.nii.gz --ref ${brain} --reg ${subject}.asl.m0.coreg.lta
-#
-# # Downsample PV images to ASL space
-# mri_vol2vol --mov ${subject}.asl.m0.nii.gz --targ ${pvgm} --lta ${subject}.asl.m0.coreg.lta --trilin --o ${pvgm_asl} --inv
-# mri_vol2vol --mov ${subject}.asl.m0.nii.gz --targ ${pvwm} --lta ${subject}.asl.m0.coreg.lta --trilin --o ${pvwm_asl} --inv
 
-# Load modules
+# Generate M0 and LC
+fslroi ${subject}.asl.nii.gz ${subject}.asl.m0.nii.gz 0 1
+fslroi ${subject}.asl.nii.gz ${subject}.asl.lc.nii.gz 1 90
 
-. /etc/profile.d/modules.sh
-module purge
-module unload fsl/5.0.10
-module load fsl/6.0.3 # Necessary, otherwise oxford_asl does not recognise iaf.
-module load freesurfer/7.1.0
-# Load modules
-module unload fsl/5.0.10
-module load fsl/6.0.3 # Necessary, otherwise oxford_asl does not recognise iaf.
+# Register M0 to T1 skull stripped brain
+mri_coreg --mov ${subject}.asl.m0.nii.gz --ref ${brain} --reg ${subject}.asl.m0.coreg.lta
+
+# Downsample PV images to ASL space
+mri_vol2vol --mov ${subject}.asl.m0.nii.gz --targ ${pvgm} --lta ${subject}.asl.m0.coreg.lta --trilin --o ${pvgm_asl} --inv
+mri_vol2vol --mov ${subject}.asl.m0.nii.gz --targ ${pvwm} --lta ${subject}.asl.m0.coreg.lta --trilin --o ${pvwm_asl} --inv
 
 # Run FSL BASIL
-  oxford_asl -i ${lc} -o basil_spatial --bolus=${bolus} --slicedt=${slicedt} --iaf=tc -c ${m0} --tis=1.8 --mc --wp --spatial --tr=2.5 --pvcorr --pvgm ${pvgm_asl} --pvwm ${pvwm_asl}
+oxford_asl -i ${lc} -o basil_spatial --bolus=${bolus} --slicedt=${slicedt} --iaf=tc -c ${m0} --tis=1.8 --mc --wp --spatial --tr=2.5 --pvcorr --pvgm ${pvgm_asl} --pvwm ${pvwm_asl}
 
 # Run FSL BASIL without spatial
 oxford_asl -i ${lc} -o basil_no_spatial --bolus=${bolus} --slicedt=${slicedt} --iaf=tc -c ${m0} --tis=1.8 --mc --wp  --tr=2.5 --pvcorr --pvgm ${pvgm_asl} --pvwm ${pvwm_asl}
