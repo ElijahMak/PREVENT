ff13c725f01ac89eaf69b82cd599d025ffd75ef2
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-11-26 09:39:57 +0000

    a

--- em_ants_reg.sh
+++ em_ants_reg.sh
@@ -10,7 +10,7 @@ module load ANTS/2.3.4
 
 # Parameters
 subject=${1}
-pet="${2}"
+pet="${1}_${2}"
 t1="${subject}.brain.nii"
 t1n4="${subject}.brain.n4.nii"
 template="/scratch/hphi/fkm24/em_code/MCALT_v1.4/MCALT_T1_brain.nii"
