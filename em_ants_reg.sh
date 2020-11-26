f076083e78667e79caa91b6d8efe572996bd184c
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-11-26 09:43:26 +0000

    a

--- em_ants_reg.sh
+++ em_ants_reg.sh
@@ -21,10 +21,10 @@ cd $subject
 N4BiasFieldCorrection -i ${t1} -o ${t1n4}
 
 # Register N4 T1 to MCALT
-antsRegistrationSyNQuick -d 3 -f ${template} -m ${t1n4} -o T1xMCALT
+antsRegistrationSyNQuick.sh -d 3 -f ${template} -m ${t1n4} -o T1xMCALT
 
 # Rigidly align PET to T1
-antsRegistrationSyNQuick -d 3 -f ${t1n4} -m ${pet}.nii -t r -o PETxT1
+antsRegistrationSyNQuick.sh -d 3 -f ${t1n4} -m ${pet}.nii -t r -o PETxT1
 
 # Warp PET to MNI
 antsApplyTransforms -d 3 -v 1 -i ${other} -r ${template} -o ${pet}_warped_to_MCALT.nii -n Linear -t T1xMCALT1Warp.nii.gz -t T1xMCALT0GenericAffine.mat -t PETxT10GenericAffine.mat
