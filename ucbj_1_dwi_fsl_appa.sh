d7e89739d71e517f5d82ad6f153833188e4e0ca0
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-11-27 12:44:12 +0000

    a

---
+++ ucbj_1_dwi_fsl_appa.sh
@@ -0,0 +1,51 @@
+DWI preprocessing, using a single B0 from PA to correct distortions in AP.
+# Author: Elijah Make
+
+# Directory
+# --------------------------------------------
+dir="/scratch/hphi/fkm24/projects/ucbjxnoddi/noddi/nba_noddi"
+
+# Modules
+# --------------------------------------------
+module unload fsl/5.0.10
+module load fsl/6.0.3
+
+# Parameters
+# --------------------------------------------
+subject=${1}
+cd $subject
+
+# # Merge B0s from AP and PAs
+# # --------------------------------------------
+# fslroi ap.nii nodif 0 1
+# fslroi pa.nii nodif_PA 0 1
+# fslmerge -t AP_PA_b0.nii nodif.nii nodif_PA.nii
+
+# Merge AP and PA
+# --------------------------------------------
+rm ap_pa.bvecs; rm ap_pa.bvals
+fslmerge -t ap_pa.nii ap.nii pa.nii
+paste -d" " ap.bvecs pa.bvecs >> ap_pa.bvecs
+paste -d" " ap.bvals pa.bvals >> ap_pa.bvecs
+
+# Create ACQPARAMS
+# --------------------------------------------
+printf "0 -1 0 0.07\n0 1 0 0.07 " > acqparams.txt
+
+# Create index
+# --------------------------------------------
+myVar=($(wc -w ap.bvals))
+indx=""
+for ((i=1; i<=$myVar; i+=1)); do indx="$indx 1"; done
+for ((i=1; i<=$myVar; i+=1)); do indx="$indx 2"; done
+echo $indx > index.txt
+
+# # Run TOPUP
+# # --------------------------------------------
+# topup --imain=AP_PA_b0 --datain=acqparams.txt --config=b02b0.cnf --out=topup_AP_PA_b0 --iout=topup_AP_PA_b0_iout --fout=topup_AP_PA_b0_fout
+#
+# # Create mean of corrected B0s
+# # --------------------------------------------
+# fslmaths topup_AP_PA_b0_iout -Tmean hifi_nodif
+
+eddy_openmp --imain=ap_pa.nii --mask=hifi_nodif_brain_mask --acqp=acqparams.txt --index=index.txt --bvecs=ap_pa.bvecs --bvals=ap_pa.bvals --out=eddy_unwarped_images_appa --verbose --data_is_shelled --fwhm=0 --flm=quadratic --repol --cnr_maps --topup=topup_AP_PA_b0
