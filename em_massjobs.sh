7f8e2f5a495dd31e768dc9c7bf9fff5d2a351529
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-12-02 05:49:59 +0000

    a

--- em_massjobs.sh
+++ em_massjobs.sh
@@ -8,10 +8,10 @@
 # Estimated time on HPHI is _ minutes
 
 # Time
-time="2:30:00"
+time="1:30:00"
 
 # Memory
-mem="4000"
+mem="1000"
 
 # Number of tasks
 nTask=1
@@ -25,7 +25,7 @@ options=${3}
 for subject in `cat $input`; do
 sbatch --job-name=em_dwi2 --account hphi --qos=long.q --partition wbic-cs --error=${subject%.*}_%j.err \
 --output=${subject%.*}_%j.out --time=${time} --nodes=1 --ntasks-per-node=${nTask} --mem=${mem} \
---wrap="bash ${code}/${script} ${subject}"
+--wrap="bash ${code}/${script} ${subject} ${options}"
 
 
 done
