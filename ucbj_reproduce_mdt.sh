97995c43fc6e1743c7083030dedfbb7bc3775972
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-12-08 14:03:51 +0000

    a

--- ucbj_reproduce_mdt.sh
+++ ucbj_reproduce_mdt.sh
@@ -40,16 +40,18 @@ mdt_odi_wm="/lustre/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/
 reg="$SUBJECTS_DIR/${subject}/noddi/coreg_hifi_nodif.lta"
 # reg="${SUBJECTS_DIR}/${subject}/ucbj2/coreg_unpvc_r1.lta"
 # seg="$SUBJECTS_DIR/${subject}/noddi/aparcaseg_2_noddi_odi.nii"
-seg="$SUBJECTS_DIR/${subject}/noddi/wmparc_2_b0_mdt.nii"
+wmparc_seg="$SUBJECTS_DIR/${subject}/noddi/wmparc_2_b0_mdt.nii"
+aparcaseg_seg="$SUBJECTS_DIR/${subject}/noddi/aparcaseg_2_noddi_odi.nii"
+
 wmparc_stats="$SUBJECTS_DIR/${subject}/stats/wmparc_2_mdt_odi_gm.dat"
 aparcaseg_stats="$SUBJECTS_DIR/${subject}/stats/aparcaseg_2_mdt_odi_gm.dat"
 
 targ="$SUBJECTS_DIR/${subject}/mri/wmparc.mgz"
 
 # NODDI
-mri_segstats --seg ${seg} --ctab $FREESURFER_HOME/FreeSurferColorLUT.txt --excludeid 0 --sum ${wmparc_stats} --i ${mdt_odi_gm}
+mri_segstats --seg ${wmparc_seg} --ctab $FREESURFER_HOME/FreeSurferColorLUT.txt --excludeid 0 --sum ${wmparc_stats} --i ${mdt_odi_gm}
 
-mri_segstats --seg ${seg} --ctab $FREESURFER_HOME/FreeSurferColorLUT.txt --excludeid 0 --sum ${aparcaseg_stats} --i ${mdt_odi_gm}
+mri_segstats --seg ${aparcaseg_seg} --ctab $FREESURFER_HOME/FreeSurferColorLUT.txt --excludeid 0 --sum ${aparcaseg_stats} --i ${mdt_odi_gm}
 
 # UCBJ
 #mri_vol2vol --mov ${mov_unvpc} --targ $SUBJECTS_DIR/${subject}/mri/aparc+aseg.mgz --inv --interp nearest --o $SUBJECTS_DIR/${subject}/ucbj2/aparcaseg_2_unpvc_ucbj.nii --reg ${reg}
