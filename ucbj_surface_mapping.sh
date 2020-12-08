43cbcd1bfe24f34d32bc1b71068be0d17daf6949
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-12-08 03:45:48 +0000

    a

--- ucbj_surface_mapping.sh
+++ ucbj_surface_mapping.sh
@@ -21,21 +21,21 @@ reg="$SUBJECTS_DIR/${subject}/noddi/coreg_hifi_nodif.lta"
 mov2="/lustre/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/noddi/${subject}/NODDI_MATLAB_v101/noddi_odi.nii.gz"
 # mov2="/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/${subject}/${subject}_UCB-J_PVC_BP_Radio.nii"
 
-#  Coregister UCBJ to T1
-#  --------------------------------------------
-mri_coreg --s ${subject} --mov ${mov} --reg ${reg}
+# #  Coregister UCBJ to T1
+# #  --------------------------------------------
+# mri_coreg --s ${subject} --mov ${mov} --reg ${reg}
 
-# Project pvc volume to fsaverage surface
-# --------------------------------------------
-for h in lh rh; do
-fsaverage_output="${SUBJECTS_DIR}/${subject}/noddi/${h}_noddi_odi_fsaverage.nii.gz"
-native_output="${SUBJECTS_DIR}/${subject}/noddi/${h}_noddi_odi_native.nii.gz"
-projfrac="0.5"
-projfrac="0.5"
-# mov="/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/${subject}/${subject}_UCB-J_PVC_BP_Radio.nii"
-# reg="${SUBJECTS_DIR}/${subject}/ucbj2/coreg_pvc_r1.lta"
-# output="${SUBJECTS_DIR}/${subject}/ucbj2/${h}_pvc_ucbj_bp.nii.gz"
-mri_vol2surf --mov $mov2 --reg $reg --hemi ${h} --projfrac 0.5 --o $fsaverage_output --cortex --trgsubject fsaverage; done
+# # Project pvc volume to fsaverage surface
+# # --------------------------------------------
+# for h in lh rh; do
+# fsaverage_output="${SUBJECTS_DIR}/${subject}/noddi/${h}_noddi_odi_fsaverage.nii.gz"
+# native_output="${SUBJECTS_DIR}/${subject}/noddi/${h}_noddi_odi_native.nii.gz"
+# projfrac="0.5"
+# projfrac="0.5"
+# # mov="/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/${subject}/${subject}_UCB-J_PVC_BP_Radio.nii"
+# # reg="${SUBJECTS_DIR}/${subject}/ucbj2/coreg_pvc_r1.lta"
+# # output="${SUBJECTS_DIR}/${subject}/ucbj2/${h}_pvc_ucbj_bp.nii.gz"
+# mri_vol2surf --mov $mov2 --reg $reg --hemi ${h} --projfrac 0.5 --o $fsaverage_output --cortex --trgsubject fsaverage; done
 
 # Project pvc volume to native surface
 # --------------------------------------------
@@ -46,7 +46,7 @@ projfrac="0.5"
 # mov="/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/${subject}/${subject}_UCB-J_PVC_BP_Radio.nii"
 # reg="${SUBJECTS_DIR}/${subject}/ucbj2/coreg_pvc_r1.lta"
 # output="${SUBJECTS_DIR}/${subject}/ucbj2/${h}_pvc_ucbj_bp_native.nii.gz"
-mri_vol2surf --mov $mov --reg $reg --hemi ${h} --projfrac 0.5 --o $native_output --cortex --trgsubject ${subject}; done
+mri_vol2surf --mov $mov2 --reg $reg --hemi ${h} --projfrac 0.5 --o $native_output --cortex --trgsubject ${subject}; done
 
 # Smooth surfaces on fsaverage
 # --------------------------------------------
@@ -86,31 +86,31 @@ mris_fwhm --smooth-only --i ${fsaverage_output} --fwhm ${fwhm} --o ${fsaverage_o
 # output="${SUBJECTS_DIR}/${subject}/ucbj2/${h}_unpvc_ucbj_bp_sm${fwhm}.nii.gz"
 # mris_fwhm --smooth-only --i $input --fwhm $fwhm --o $output  --cortex --s fsaverage --hemi ${h}; done
 
-# Derive volumes
-# --------------------------------------------
-# mov="/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/${subject}/${subject}_UCB-J_PVC_BP_Radio.nii"
-# reg="${SUBJECTS_DIR}/${subject}/ucbj2/coreg_pvc_r1.lta"
-targ="$SUBJECTS_DIR/${subject}/mri/brainmask.mgz"
-output_volume="${SUBJECTS_DIR}/${subject}/noddi/vol_noddi_odi.nii.gz"
-mni152_output="${SUBJECTS_DIR}/${subject}/noddi/mni_vol_noddi_odi.nii.gz"
-# output_volume="${SUBJECTS_DIR}/${subject}/ucbj2/vol_pvc_ucbj_bp.nii.gz"
-# mni152_output="${SUBJECTS_DIR}/${subject}/ucbj2/mni_vol_pvc_ucbj_bp.nii.gz"
-sm_output="${SUBJECTS_DIR}/${subject}/noddi/vol_noddi_odi_sm${fwhm}.nii.gz"
-sm_mni152_output="${SUBJECTS_DIR}/${subject}/noddi/mni_vol_noddi_odi_sm${fwhm}.nii.gz"
-fwhm="8"
-
-# Derive PET volume in native T1
-# --------------------------------------------
-mri_vol2vol --mov ${mov2} --reg ${reg} --targ ${targ} --o ${output_volume}
-
-# Derive PET volume in MNI152 2MM
-# --------------------------------------------
-mri_vol2vol --mov ${mov2} --reg ${reg} --targ ${targ} --o ${mni152_output} --mni152reg
-
-# Smooth PET volumes in T1 and MNI152 spaces
-# --------------------------------------------
-mri_fwhm --smooth-only --i ${output_volume} --fwhm ${fwhm} --o ${sm_output}
-mri_fwhm --smooth-only --i ${mni152_output} --fwhm $fwhm --o ${sm_mni152_output}
+# # Derive volumes
+# # --------------------------------------------
+# # mov="/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/${subject}/${subject}_UCB-J_PVC_BP_Radio.nii"
+# # reg="${SUBJECTS_DIR}/${subject}/ucbj2/coreg_pvc_r1.lta"
+# targ="$SUBJECTS_DIR/${subject}/mri/brainmask.mgz"
+# output_volume="${SUBJECTS_DIR}/${subject}/noddi/vol_noddi_odi.nii.gz"
+# mni152_output="${SUBJECTS_DIR}/${subject}/noddi/mni_vol_noddi_odi.nii.gz"
+# # output_volume="${SUBJECTS_DIR}/${subject}/ucbj2/vol_pvc_ucbj_bp.nii.gz"
+# # mni152_output="${SUBJECTS_DIR}/${subject}/ucbj2/mni_vol_pvc_ucbj_bp.nii.gz"
+# sm_output="${SUBJECTS_DIR}/${subject}/noddi/vol_noddi_odi_sm${fwhm}.nii.gz"
+# sm_mni152_output="${SUBJECTS_DIR}/${subject}/noddi/mni_vol_noddi_odi_sm${fwhm}.nii.gz"
+# fwhm="8"
+#
+# # Derive PET volume in native T1
+# # --------------------------------------------
+# mri_vol2vol --mov ${mov2} --reg ${reg} --targ ${targ} --o ${output_volume}
+#
+# # Derive PET volume in MNI152 2MM
+# # --------------------------------------------
+# mri_vol2vol --mov ${mov2} --reg ${reg} --targ ${targ} --o ${mni152_output} --mni152reg
+#
+# # Smooth PET volumes in T1 and MNI152 spaces
+# # --------------------------------------------
+# mri_fwhm --smooth-only --i ${output_volume} --fwhm ${fwhm} --o ${sm_output}
+# mri_fwhm --smooth-only --i ${mni152_output} --fwhm $fwhm --o ${sm_mni152_output}
 
 # # Project UNPVC volume
 # # --------------------------------------------
