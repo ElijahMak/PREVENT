a7cd6f122cfac13f21051b52fdfaba2c809e019a
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-12-08 02:42:48 +0000

    a

--- em_massjobs.sh
+++ em_massjobs.sh
@@ -19,13 +19,14 @@ nTask=1
 # List of subjects to process
 input=${1}
 script=${2}
-options=${3}
+task=${3}
+options=${4}
 
 # Execute job submission
 for subject in `cat $input`; do
-sbatch --job-name=em_dwi2 --account hphi --qos=long.q --partition wbic-cs --error=${subject%.*}_%j.err \
---output=${subject%.*}_%j.out --time=${time} --nodes=1 --ntasks-per-node=${nTask} --mem=${mem} \
---wrap="bash ${script} ${subject} ${options}"
+sbatch --job-name=haste --account hphi --qos=long.q --partition wbic-cs --error=${subject}/${subject%.*}_%x.err \
+--output=${subject}/${subject%.*}_%j.out --time=${time} --nodes=1 --ntasks-per-node=${nTask} --mem=${mem} \
+--wrap="bash ${script} ${subject} ${task}"
 
 
 done
