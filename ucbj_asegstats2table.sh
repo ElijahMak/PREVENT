7c8fb718f017fbaef901e29ef7e0bcc61b48e934
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-12-08 12:43:01 +0000

    a

--- ucbj_asegstats2table.sh
+++ ucbj_asegstats2table.sh
@@ -17,18 +17,18 @@ module load MRtrix/mrtrix-3.0.2
 SUBJECTS_DIR="/lustre/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/freesurfer"
 subjectsfile=${1}
 
-# Compute surface data
-# --------------------------------------------
-for h in lh rh; do
-  for x in noddi_odi_native unpvc_ucbj_bp_native; do
-  asegstats2table --subjectsfile ${subjectsfile} --meas mean --stats=${h}_${x}.dat --table ${h}_${x}.csv  --all-segs
-done
-done
-
-# Merge files
-for x in noddi_odi_native unpvc_ucbj_bp_native; do
-cat lh_${x}.csv rh_${x}.csv > lh_rh_${x}.csv
-done
+# # Compute surface data
+# # --------------------------------------------
+# for h in lh rh; do
+#   for x in noddi_odi_native unpvc_ucbj_bp_native; do
+#   asegstats2table --subjectsfile ${subjectsfile} --meas mean --stats=${h}_${x}.dat --table ${h}_${x}.csv  --all-segs
+# done
+# done
+#
+# # Merge files
+# for x in noddi_odi_native unpvc_ucbj_bp_native; do
+# cat lh_${x}.csv rh_${x}.csv > lh_rh_${x}.csv
+# done
 
 # Compute aparcaseg data
 # --------------------------------------------
@@ -36,7 +36,8 @@ for x in aparcaseg_2_noddi_odi aparcaseg_2_unpvc_ucbj; do
   asegstats2table --subjectsfile ${subjectsfile} --meas mean --stats=${x}.dat --table ${x}.csv --all-segs
 done
 
-# Merge files
-for x in aparcaseg_2_noddi_odi aparcaseg_2_unpvc_ucbj; do
-cat ${x}.csv ${x}.csv > lh_rh_${x}.csv
+# Compute wmparc data
+# --------------------------------------------
+for x in wmparc_2_noddi_odi ; do
+  asegstats2table --subjectsfile ${subjectsfile} --meas mean --stats=${x}.dat --table ${x}.csv --all-segs
 done
