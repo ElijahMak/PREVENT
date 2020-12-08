41b893a73709dd26ec757572573e0cefb3dee963
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-12-08 20:53:40 +0000

    a

--- ucbj_surface_mrisegstats.sh
+++ ucbj_surface_mrisegstats.sh
@@ -16,30 +16,29 @@ module load MRtrix/mrtrix-3.0.2
 subject=${1}
 
 SUBJECTS_DIR="/lustre/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/freesurfer"
-
-for h in lh rh; do
-mri_segstats --annot ${subject} ${h} aparc --i ${subject}/ucbj2/${h}_unpvc_ucbj_bp_native.nii.gz --sum ${subject}/stats/${h}_unpvc_ucbj_bp_native.dat
-done
-
-for h in lh rh; do
-mri_segstats --annot ${subject} ${h} aparc --i ${subject}/ucbj2/${h}_pvc_ucbj_bp_native.nii.gz --sum ${subject}/stats/${h}_pvc_ucbj_bp_native.dat
-done
-
-for h in lh rh; do
-mri_segstats --annot ${subject} ${h} aparc --i ${subject}/noddi/${h}_noddi_odi_native.nii.gz --sum ${subject}/stats/${h}_noddi_odi_native.dat
-done
+#
+# for h in lh rh; do
+# mri_segstats --annot ${subject} ${h} aparc --i ${subject}/ucbj2/${h}_unpvc_ucbj_bp_native.nii.gz --sum ${subject}/stats/${h}_unpvc_ucbj_bp_native.dat
+# done
+#
+# for h in lh rh; do
+# mri_segstats --annot ${subject} ${h} aparc --i ${subject}/ucbj2/${h}_pvc_ucbj_bp_native.nii.gz --sum ${subject}/stats/${h}_pvc_ucbj_bp_native.dat
+# done
+#
+# for h in lh rh; do
+# mri_segstats --annot ${subject} ${h} aparc --i ${subject}/noddi/${h}_noddi_odi_native.nii.gz --sum ${subject}/stats/${h}_noddi_odi_native.dat
+# done
 
 # Parameters
 # --------------------------------------------
 mov="/lustre/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/noddi/${subject}/hifi_nodif.nii"
-mov_pvc="/lustre/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/${subject}/${subject}_UCB-J_PVC_BP_Radio.nii"
-mov_unpvc="/lustre/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/${subject}/${subject}_UCB-J_BP_Radio.nii"
+# mov_pvc="/lustre/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/${subject}/${subject}_UCB-J_PVC_BP_Radio.nii"
+# mov_unpvc="/lustre/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/${subject}/${subject}_UCB-J_BP_Radio.nii"
 reg="$SUBJECTS_DIR/${subject}/noddi/coreg_hifi_nodif.lta"
 # reg="${SUBJECTS_DIR}/${subject}/ucbj2/coreg_unpvc_r1.lta"
-# seg="$SUBJECTS_DIR/${subject}/noddi/aparcaseg_2_noddi_odi.nii"
-seg="$SUBJECTS_DIR/${subject}/noddi/wmparc_2_b0.nii"
-stats="$SUBJECTS_DIR/${subject}/stats/wmparc_2_noddi_odi.dat"
-targ="$SUBJECTS_DIR/${subject}/mri/wmparc.mgz"
+seg="$SUBJECTS_DIR/${subject}/noddi/aparcaseg_2_noddi_b0.nii"
+stats="$SUBJECTS_DIR/${subject}/stats/aparcaseg_2_noddi_odi.dat"
+targ="$SUBJECTS_DIR/${subject}/mri/aparc+aseg.mgz"
 odi="/lustre/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/noddi/${subject}/NODDI_MATLAB_v101/noddi_odi.nii.gz"
 
 # NODDI
