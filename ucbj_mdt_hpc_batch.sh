f771ba08fdca1d6665b10be17e58dfd0fc6d7dc7
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-12-09 14:50:04 +0000

    a

--- ucbj_mdt_hpc_batch.sh
+++ ucbj_mdt_hpc_batch.sh
@@ -16,8 +16,7 @@ do
 mem="2000"
 time="02:30:00"
 
-sbatch --gres=gpu:1 --job-name=noddi --account MAK-SL3-GPU --partition pascal --error$
---output=${subject%.*}_%j.out --time=$time --nodes=1 --ntasks-per-node=$nTask --mem=$$
+sbatch --gres=gpu:1 --job-name=noddi --account MAK-SL3-GPU --partition pascal --output=${subject%.*}_%x.out --time=$time --nodes=1 --ntasks-per-node=$nTask --mem=$$
 --wrap="bash /home/fkm24/em_code/ucbj_mdt_hpc.sh ${subject}" \
 --mail-type=ALL
 
