0e675ef08a6d1ab2ef28c5aa6da9d29409cc936a
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-12-09 18:11:51 +0000

    a

---
+++ ucbj_mdt_hpc_n1_batch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Example: bash code/em_noddi_batch.sh list v1(or v2)
+
+module load openmpi-4.0.0-intel-17.0.8-tnwn5io
+module load cuda/8.0
+
+# Number of tasks
+nTask=1
+
+ListOfSubjects="${1}"
+
+for subject in `cat $ListOfSubjects`;
+
+do
+mem="2000"
+time="00:30:00"
+
+sbatch --gres=gpu:1 --job-name=noddi --account MAK-SL3-GPU --partition pascal --output=${subject%.*}_%x.out --time=$time --nodes=1 --ntasks-per-node=$nTask --mem=$mem --wrap="bash /home/fkm24/em_code/ucbj_mdt_n1.sh ${subject}"
+done
+sleep 1
---
+++ ucbj_mdt_n1.sh
@@ -0,0 +1,31 @@
+# Process DWI dataset for the extraction of NODDI maps in HPHI
+# Author: Elijah Mak
+
+
+# Load modules
+# ---------------------------------------------------
+source ~/mdt/bin/activate
+
+# Parameters
+# ---------------------------------------------------
+subject=${1}
+cd ${subject}
+
+mask="hifi_nodif_brain_f0.3_mask.nii"
+input="eddy_unwarped_images.nii"
+rotated_bvecs="eddy_unwarped_images.eddy_rotated_bvecs"
+bval="ap.bvals"
+prtcl="mdt.prtcl"
+
+# Create protocol
+# ---------------------------------------------------
+mdt-create-protocol ${rotated_bvecs} ${bval} -o ${prtcl}
+
+# Remove ocassional locks
+# ---------------------------------------------------
+rm -rf /home/fkm24/.cache/pyopencl/pyopencl-compiler-cache-v2-py3.6.5.final.0/lock
+
+# Fit NODDI models
+# ---------------------------------------------------
+mkdir mdt_noise1
+mdt-model-fit NODDI_GM $input mdt.prtcl ${mask} -o mdt_noise1 -n 1
