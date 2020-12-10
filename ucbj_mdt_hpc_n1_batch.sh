48e403dc339a8ebb82cc3be472a6941049fe25f5
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-12-10 09:10:31 +0000

    a

--- ucbj_mdt_hpc_n1_batch.sh
+++ ucbj_mdt_hpc_n1_batch.sh
@@ -16,6 +16,6 @@ do
 mem="2000"
 time="01:00:00"
 
-sbatch --gres=gpu:1 --job-name=noddi --account MAK-SL3-GPU --partition pascal --output=${subject%.*}_%x.out --time=$time --nodes=1 --ntasks-per-node=$nTask --mem=$mem --wrap="bash /home/fkm24/em_code/ucbj_mdt_n1.sh ${subject}"
+sbatch --gres=gpu:1 --job-name=noddi_n1 --account MAK-SL3-GPU --partition pascal --output=${subject%.*}_%x.out --time=$time --nodes=1 --ntasks-per-node=$nTask --mem=$mem --wrap="bash /home/fkm24/em_code/ucbj_mdt_n1.sh ${subject}"
 done
 sleep 1
