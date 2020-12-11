ae3018c0209f1877b0dc4e532339d5159720ca1f
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-12-11 10:53:09 +0000

    a

--- em_mrtrix_batch.sh
+++ em_mrtrix_batch.sh
@@ -22,7 +22,7 @@ input=${1}
 # Execute job submission
 for subject in `cat $input`; do
 
-sbatch --job-name=dwi_ap --account hphi --qos=long.q --partition wbic-cs --error=${subject%.*}_%j.err \
+sbatch --job-name=mrtrixem --account hphi --qos=long.q --partition wbic-cs --error=${subject%.*}_%j.err \
 --output=${subject%.*}_%j.out --time=${time} --nodes=1 --cpus-per-task=5 --ntasks-per-node=${nTask} --mem=${mem} \
 --wrap="bash ${code}/em_mrtrix.sh ${subject}"
 
