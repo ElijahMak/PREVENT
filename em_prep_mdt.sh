6374dba3424fb62104ec94dd3106270c498dfbd2
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-12-11 11:11:03 +0000

    a

---
+++ em_prep_mdt.sh
@@ -0,0 +1,6 @@
+subject = ${1}
+
+cp ${i}/eddy_unwarped_images.nii hpc/${i}
+cp ${i}/eddy_unwarped_images.eddy_rotated_bvecs hpc/${i}
+cp ${i}/ap.bvals hpc/${i}
+cp ${i}/hifi_nodif_brain_f0.3_mask.nii hpc/${i}
