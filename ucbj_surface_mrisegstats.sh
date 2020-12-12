067d1827b30cd114e85c6ea48629dc62c03a7bb3
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-12-12 16:53:45 +0000

    a

--- ucbj_surface_mrisegstats.sh
+++ ucbj_surface_mrisegstats.sh
@@ -25,7 +25,7 @@ done
 # ODI
 # --------------------------------------------
 for h in lh rh; do
-mri_segstats --annot ${subject} ${h} aparc --i ${subject}/noddi/${h}_mdt_odi_native.nii.gz --sum ${subject}/stats/${h}_mdt_odi_native.dat
+  mri_segstats --annot ${subject} ${h} aparc --i ${subject}/noddi/${h}_mdt_odi.nii.gz --sum ${subject}/stats/${h}_mdt_odi.dat
 done
 
 # Volumetric data
