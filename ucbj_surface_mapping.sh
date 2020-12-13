21e740d7c6041586888bac8d8ee1a6881d41555b
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-12-13 09:57:34 +0000

    a

--- ucbj_surface_mapping.sh
+++ ucbj_surface_mapping.sh
@@ -24,7 +24,7 @@ subject=${1}
 mov="/lustre/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/noddi2/${subject}/hifi_nodif.nii"
 # Use coreg files
 reg="$SUBJECTS_DIR/${subject}/noddi/coreg_hifi_nodif.lta"
-odi="/lustre/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/noddi/mdt/${subject}/mdt/NODDI_GM/ODI.nii.gz"
+odi="/lustre/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/noddi2/${subject}/mdt/NODDI_GM/ODI.nii.gz"
 
 
 mri_coreg --s ${subject} --mov ${mov} --reg ${reg}
@@ -41,38 +41,38 @@ mri_vol2surf --mov $odi --reg $reg --hemi ${h} --projfrac 0.5 --o $fsaverage_out
 mri_vol2surf --mov $odi --reg $reg --hemi ${h} --projfrac 0.5 --o $native_output --cortex --trgsubject ${subject}
 mris_fwhm --smooth-only --i ${fsaverage_output} --fwhm ${fwhm} --o ${fsaverage_output_smooth} --cortex --s fsaverage --hemi ${h}; done
 
-
-# --------------------------------------------
-# UCBJ
-# --------------------------------------------
-
-# Parameters
-# --------------------------------------------
-SUBJECTS_DIR="/lustre/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/freesurfer"
-subject=${1}
-# Use b0 as warps
-mov_pvc="/lustre/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/${subject}/${subject}_UCB-J_PVC_BP_Radio.nii"
-# Use coreg files
-reg="$SUBJECTS_DIR/${subject}/ucbj2/coreg_pvc_r1.lta"
-
-# Coregister UCBJ to T1
-# --------------------------------------------
-# mri_coreg --s ${subject} --mov /archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/${subject}/${subject}_UCB-J_PVC_R1_Radio.nii --reg $SUBJECTS_DIR/${subject}/ucbj2/coreg_pvc_r1.lta
-
-# Project UCBJ to fsaverage and native surfaces
-# --------------------------------------------
-for h in lh rh; do
-fwhm="8"
-mov_pvc="/lustre/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/${subject}/${subject}_UCB-J_PVC_BP_Radio.nii"
-reg="$SUBJECTS_DIR/${subject}/ucbj2/coreg_pvc_r1.lta"
-projfrac="0.5"
-fsaverage_output="${SUBJECTS_DIR}/${subject}/ucbj2/${h}_pvc_ucbj_bp_fsaverage.nii.gz"
-native_output="${SUBJECTS_DIR}/${subject}/ucbj2/${h}_pvc_ucbj_bp.nii.gz"
-fsaverage_output_smooth="${SUBJECTS_DIR}/${subject}/ucbj2/${h}_pvc_ucbj_bp_fsaverage_sm${fwhm}.nii.gz"
-mri_vol2surf --mov $mov_pvc --reg $reg --hemi ${h} --projfrac 0.5 --o $fsaverage_output --cortex --trgsubject fsaverage
-mri_vol2surf --mov $mov_pvc --reg $reg --hemi ${h} --projfrac 0.5 --o $native_output --cortex --trgsubject ${subject}
-mris_fwhm --smooth-only --i $fsaverage_output --fwhm $fwhm --o $fsaverage_output_smooth  --cortex --s fsaverage --hemi ${h}
-done
+#
+# # --------------------------------------------
+# # UCBJ
+# # --------------------------------------------
+#
+# # Parameters
+# # --------------------------------------------
+# SUBJECTS_DIR="/lustre/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/freesurfer"
+# subject=${1}
+# # Use b0 as warps
+# mov_pvc="/lustre/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/${subject}/${subject}_UCB-J_PVC_BP_Radio.nii"
+# # Use coreg files
+# reg="$SUBJECTS_DIR/${subject}/ucbj2/coreg_pvc_r1.lta"
+#
+# # Coregister UCBJ to T1
+# # --------------------------------------------
+# # mri_coreg --s ${subject} --mov /archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/${subject}/${subject}_UCB-J_PVC_R1_Radio.nii --reg $SUBJECTS_DIR/${subject}/ucbj2/coreg_pvc_r1.lta
+#
+# # Project UCBJ to fsaverage and native surfaces
+# # --------------------------------------------
+# for h in lh rh; do
+# fwhm="8"
+# mov_pvc="/lustre/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/${subject}/${subject}_UCB-J_PVC_BP_Radio.nii"
+# reg="$SUBJECTS_DIR/${subject}/ucbj2/coreg_pvc_r1.lta"
+# projfrac="0.5"
+# fsaverage_output="${SUBJECTS_DIR}/${subject}/ucbj2/${h}_pvc_ucbj_bp_fsaverage.nii.gz"
+# native_output="${SUBJECTS_DIR}/${subject}/ucbj2/${h}_pvc_ucbj_bp.nii.gz"
+# fsaverage_output_smooth="${SUBJECTS_DIR}/${subject}/ucbj2/${h}_pvc_ucbj_bp_fsaverage_sm${fwhm}.nii.gz"
+# mri_vol2surf --mov $mov_pvc --reg $reg --hemi ${h} --projfrac 0.5 --o $fsaverage_output --cortex --trgsubject fsaverage
+# mri_vol2surf --mov $mov_pvc --reg $reg --hemi ${h} --projfrac 0.5 --o $native_output --cortex --trgsubject ${subject}
+# mris_fwhm --smooth-only --i $fsaverage_output --fwhm $fwhm --o $fsaverage_output_smooth  --cortex --s fsaverage --hemi ${h}
+# done
 
 # --------------------------------------------
 # Derive volumes
@@ -84,11 +84,11 @@ reg="$SUBJECTS_DIR/${subject}/noddi/coreg_hifi_nodif.lta"
 targ="$SUBJECTS_DIR/${subject}/mri/brainmask.mgz"
 output_volume="$SUBJECTS_DIR/${subject}/noddi2/vol_mdt_odi_anat.nii"
 mri_vol2vol --mov ${odi} --reg ${reg} --targ ${targ} --o ${output_volume}
-
-# Derive UCBJ volume in native T1
-# --------------------------------------------
-reg="$SUBJECTS_DIR/${subject}/ucbj2/coreg_pvc_r1.lta"
-targ="$SUBJECTS_DIR/${subject}/mri/brainmask.mgz"
-mov_pvc="/lustre/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/${subject}/${subject}_UCB-J_PVC_BP_Radio.nii"
-output_volume="$SUBJECTS_DIR/${subject}/ucbj2/vol_pvc_ucbj_bp_anat.nii"
-mri_vol2vol --mov ${mov_pvc} --reg ${reg} --targ ${targ} --o ${output_volume}
+#
+# # Derive UCBJ volume in native T1
+# # --------------------------------------------
+# reg="$SUBJECTS_DIR/${subject}/ucbj2/coreg_pvc_r1.lta"
+# targ="$SUBJECTS_DIR/${subject}/mri/brainmask.mgz"
+# mov_pvc="/lustre/archive/p00423/PREVENT_Elijah/NeurobiologyAgeing_UCBJXNODDI/${subject}/${subject}_UCB-J_PVC_BP_Radio.nii"
+# output_volume="$SUBJECTS_DIR/${subject}/ucbj2/vol_pvc_ucbj_bp_anat.nii"
+# mri_vol2vol --mov ${mov_pvc} --reg ${reg} --targ ${targ} --o ${output_volume}
