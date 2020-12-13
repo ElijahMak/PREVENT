195c5612c4efc0b797f6c4d59320e9111949263f
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-12-13 18:24:46 +0000

    a

--- ucbj_1_dwi_ap_batch.sh
+++ ucbj_1_dwi_ap_batch.sh
@@ -22,8 +22,8 @@ input=${1}
 # Execute job submission
 for subject in `cat $input`; do
 
-sbatch --job-name=dwi_ap --account hphi --qos=long.q --partition wbic-cs --error=${subject%.*}_%j.err \
---output=${subject%.*}_%j.out --time=${time} --nodes=1 --ntasks-per-node=${nTask} --mem=${mem} \
+sbatch --job-name=dwi_appa --account hphi --qos=long.q --partition wbic-cs --error=${subject%.*}_%k.err \
+--output=${subject%.*}_%k.out --time=${time} --nodes=1 --ntasks-per-node=${nTask} --mem=${mem} \
 --wrap="bash ${code}/ucbj_1_dwi_fsl_appa.sh ${subject}"
 
 done
