c3e6f8ebaca6ef8dfffeceb7fff3b8855954eeee
Elijah Mak <fkm24@medschl.cam.ac.uk>
2020-12-14 12:38:36 +0000

    a

---
+++ ucbj_mdt_v2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Example: bash code/em_noddi_batch.sh list v1(or v2)
+
+module load openmpi-4.0.0-intel-17.0.8-tnwn5io
+module load cuda/8.0
+
+# Number of tasks
+nTask=1
+
+ListOfSubjects="${1}"
+
+for subject in `cat $ListOfSubjects`;
+
+do
+mem="3000"
+time="02:00:00"
+
+sbatch --gres=gpu:1 --job-name=noddidenoise --account MAK-SL3-GPU --partition pascal --output=${subject%.*}_%x.out --time=$time --nodes=1 --ntasks-per-node=$nTask --mem=$mem --wrap="bash /home/fkm24/em_code/ucbj_mdt_hpc.sh ${subject}"
+done
+sleep 1
